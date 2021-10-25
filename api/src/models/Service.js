35505d3dbfb313175ec43e8e67b28c2e3fb07764
Federico <fede.gallina94@gmail.com>
2021-10-25 16:00:59 -0300

    fix(api): change name services to service

--- api/src/models/Service.js
+++ api/src/models/Service.js
@@ -1,23 +1,23 @@
-const { DataTypes } = require('sequelize');
+const { DataTypes } = require("sequelize");
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('services', {
+  sequelize.define("service", {
     title: {
       type: DataTypes.STRING,
       allowNull: false,
     },
-    img : {
-      type : DataTypes.STRING,
-      allowNull: false
+    img: {
+      type: DataTypes.STRING,
+      allowNull: false,
     },
     description: {
-      type : DataTypes.TEXT,
-      allowNull: false
+      type: DataTypes.TEXT,
+      allowNull: false,
     },
-    price : {
-      type : DataTypes.FLOAT
-    }
+    price: {
+      type: DataTypes.FLOAT,
+    },
   });
 };
