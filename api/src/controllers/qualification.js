ebb856755b32c2f2440eace3470b66f9c186e4da
Facundo Pellicer <facundopellicer4@gmail.com>
2021-10-26 12:25:45 -0300

    feat(api/routes)delete_comment

--- api/src/controllers/qualification.js
+++ api/src/controllers/qualification.js
@@ -13,6 +13,28 @@ async function postComment(req, res, next) {
   }
 }
 
+async function deleteComment(req,res,next){
+  try {
+    const { id } = req.params;
+    const commentInDb = await Qualification.findOne({
+      // chequeo si existe el usuario
+      where: { id: id },
+    });
+    if (commentInDb === null) {
+      res.json({ respones: "comment not founded" });
+    } else {
+      await Qualification.destroy({
+        // si existe lo deleteo
+        where: { id: id },
+      });
+      res.json({ response: "comment deleted" });
+    }
+  } catch (e) {
+    next(e);
+  }
+}
+
 module.exports = {
   postComment,
+  deleteComment,
 };
--- api/src/routes/qualification.js
+++ api/src/routes/qualification.js
@@ -1,7 +1,8 @@
 let express = require('express');
 let router = express.Router();
-var { postComment } = require('../controllers/qualification');
+var { postComment, deleteComment } = require('../controllers/qualification');
 
 router.post('/', postComment);
+router.delete('/:id', deleteComment)
 
 module.exports = router;
