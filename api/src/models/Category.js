49bb13d2af85a2302a89a2abb4ca03a8c45a926c
Jonatan Segovia <jsegovia.ush@gmail.com>
2021-10-26 08:08:25 -0300

    Api/models/Category.js realizado (#10)

---
+++ api/src/models/Category.js
@@ -0,0 +1,11 @@
+const { DataTypes } = require('sequelize');
+
+module.exports = (sequelize) => {
+  sequelize.define('category', {
+    //permitimos que el id se genere automáticamente?
+    name: {
+      type: DataTypes.STRING,
+      alowNull: false,
+    },
+  });
+};
