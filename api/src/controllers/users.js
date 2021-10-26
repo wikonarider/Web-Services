32a31f16d9eee189a9dd57b47be6f63495e82eb2
Jonatan Segovia <jsegovia.ush@gmail.com>
2021-10-26 19:05:53 -0300

    Feat api/post purchase (#29)
    * feat-api/POST_purchase rutas necesarias creadas
    
    * feat-api/POST_purchase lógica para adquirir servicio/user en db ok
    
    * feat-api/POST_purchase usuario asociado al servicio correctamente
    
    * fix-api/POST_purchase modifiqué force a true y modelo

--- api/src/controllers/users.js
+++ api/src/controllers/users.js
@@ -1,5 +1,5 @@
-const { Users, Service } = require("../db");
-const { validateUser, checkUnique } = require("../utils/validUser");
+const { Users, Service } = require('../db');
+const { validateUser, checkUnique } = require('../utils/validUser');
 
 async function userCreated(req, res, next) {
   try {
@@ -10,7 +10,7 @@ async function userCreated(req, res, next) {
       const errors = validateUser(req.body);
       if (!Object.keys(errors).length) {
         await Users.create(req.body);
-        res.json({ response: "created" }); // responde con 200, y created
+        res.json({ response: 'created' }); // responde con 200, y created
       } else {
         res.status(400).json({ response: errors });
         // algun parametro invalido.
@@ -18,7 +18,7 @@ async function userCreated(req, res, next) {
     } else {
       res
         .status(400)
-        .json({ response: "username or email already exist or is empty" });
+        .json({ response: 'username or email already exist or is empty' });
     }
   } catch (e) {
     next(e);
@@ -29,28 +29,36 @@ async function getUsers(req, res, next) {
   try {
     // let { username } = req.params;
     let { username } = req.query;
-    if(!username){
+    if (!username) {
       const usersDb = await Users.findAll({
-        include: [{
-          model: Service,
-          attributes: [],
-        },],
+        include: [
+          {
+            model: Service,
+            attributes: [],
+          },
+        ],
       });
-    usersDb.length > 0 ? res.status(200).send(usersDb): res.status(500).send({ response: "no users yet"})
-    } else { const userFinded = await Users.findOne({
-      where: {
-        username,
-      }, include: [{
-        model: Service,
-        attributes: [],
-      },],
-    });
-    res.status(200).send(userFinded);
-  }; 
+      usersDb.length > 0
+        ? res.status(200).send(usersDb)
+        : res.status(500).send({ response: 'no users yet' });
+    } else {
+      const userFinded = await Users.findOne({
+        where: {
+          username,
+        },
+        include: [
+          {
+            model: Service,
+            attributes: [],
+          },
+        ],
+      });
+      res.status(200).send(userFinded);
+    }
   } catch (e) {
     next(e);
   }
-};
+}
 
 async function userBanned(req, res, next) {
   try {
@@ -60,14 +68,53 @@ async function userBanned(req, res, next) {
       where: { id: id },
     });
     if (usersInDb === null) {
-      res.json({ respones: "user not founded" });
+      res.json({ respones: 'user not founded' });
     } else {
-      await Users.update({ // si existe seteo el ban en true
-        ban: true
-      }, {
-        where: {id: id}
+      await Users.update(
+        {
+          // si existe seteo el ban en true
+          ban: true,
+        },
+        {
+          where: { id: id },
+        }
+      );
+      res.json({ response: 'user banned' });
+    }
+  } catch (e) {
+    next(e);
+  }
+}
+
+async function postPurchase(req, res, next) {
+  //necesitamos estos datos para asociar el servicio comprado a la categoría
+  const { userId, serviceId, logged } = req.body;
+
+  try {
+    if (logged === true) {
+      const userFound = await Users.findOne({
+        // busco el usuario
+        where: { id: userId },
       });
-      res.json({ response: "user banned" });
+
+      const serviceFound = await Service.findOne({
+        // busco el servicio
+        where: { id: serviceId },
+      });
+
+      //si usuario y servicio existe los asocio
+      if (userFound && serviceFound) {
+        await userFound.addService(serviceFound);
+
+        return res
+          .status(200)
+          .send({ message: 'User associated to Service successful' });
+      } else {
+        //sino existe el usuario no está logueado
+        return res
+          .status(200)
+          .send({ message: 'You need to be logged to purchase a service' });
+      }
     }
   } catch (e) {
     next(e);
@@ -77,5 +124,6 @@ async function userBanned(req, res, next) {
 module.exports = {
   userCreated,
   userBanned,
-  getUsers
+  getUsers,
+  postPurchase,
 };
--- api/src/models/Users.js
+++ api/src/models/Users.js
@@ -42,6 +42,6 @@ module.exports = (sequelize) => {
       type: DataTypes.BOOLEAN,
       allowNull: false,
       defaultValue: false,
-    }
+    },
   });
 };
