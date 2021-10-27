7e974503b05dcf63e939a845005d43d45cbf2a0a
Facundo Pellicer <facundopellicer4@gmail.com>
2021-10-27 11:23:25 -0300

    feat(client)add account buttons and account info

--- client/src/redux/reducer/index.js
+++ client/src/redux/reducer/index.js
@@ -1,19 +1,19 @@
 import { GET_SERVICES } from "../actions/variables";
 const initialState = {
   services: [],
+  userData: {},
 };
-
 
 const rootReducer = (state = initialState, action) => {
   switch (action.type) {
     case GET_SERVICES:
-      console.log('Payload en reducer',action.payload)
-      console.log('globalState de servicios',state.services)
+      console.log("Payload en reducer", action.payload);
+      console.log("globalState de servicios", state.services);
       return {
         ...state,
-        services : action.payload
+        services: action.payload,
       };
-      default:
+    default:
       return state;
   }
 };
