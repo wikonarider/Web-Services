93d9f9d388dc74ef47b03e2c0582e9a07c23f61f
Fedex159 <fede.gallina94@gmail.com>
2021-10-27 18:27:56 -0300

    api/mockup: add group mockup

---
+++ api/mock/groups.js
@@ -0,0 +1,34 @@
+const groups = [
+  {
+    id: 1,
+    name: "Clases - Cursos",
+  },
+  {
+    id: 2,
+    name: "Reparaciones - Tecnicos",
+  },
+  {
+    id: 3,
+    name: "Organizaciones de eventos",
+  },
+  {
+    id: 4,
+    name: "Transporte - Mudanzas",
+  },
+  {
+    id: 5,
+    name: "Servicios Doméstico - Limpieza",
+  },
+  {
+    id: 6,
+    name: "Turismo - Paquetes turisticos",
+  },
+  {
+    id: 7,
+    name: "Otros servicios",
+  },
+];
+
+module.exports = {
+  groups,
+};
