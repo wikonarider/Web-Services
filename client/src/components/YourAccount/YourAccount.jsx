3ac07dca967a29d20a3430f67f01935e49a818bb
Facundo <86330512+Facupelli@users.noreply.github.com>
2021-10-27 15:15:24 -0300

    feat(client)update_account_buttons (#44)

--- client/src/components/YourAccount/YourAccount.jsx
+++ client/src/components/YourAccount/YourAccount.jsx
@@ -8,15 +8,30 @@ import FavoriteIcon from "@mui/icons-material/Favorite";
 import HomeRepairServiceIcon from "@mui/icons-material/HomeRepairService";
 import ShoppingBagIcon from "@mui/icons-material/ShoppingBag";
 import PhotoCameraIcon from "@mui/icons-material/PhotoCamera";
+import DataSaverOffIcon from '@mui/icons-material/DataSaverOff';
+import VisibilityIcon from '@mui/icons-material/Visibility';
+
+//mockup de usuario para mostrar el panel
+const userData = {
+  id: "4e218c00-36ad-11ec-8d3d-0242ac130003",
+  userImg: "http://dummyimage.com/189x100.png/5fa2dd/ffffff",
+  name: "Everett",
+  lastname: "Roskilly",
+  username: "eroskilly7",
+  password: "DJM1j7OZYlO",
+  email: "eroskilly7@twitpic.com",
+  location: ["Ushuaia"],
+  ban: false,
+  createdAt: "2021-10-27T15:40:21.861Z",
+  updatedAt: "2021-10-27T15:40:21.861Z",
+};
 
 export default function YourAccount() {
-  const userData = useSelector((state) => state.userData);
+  // const userData = useSelector((state) => state.userData);
 
   return (
     <div>
-      <p className={s.yourAccount}>
-        Your Account
-      </p>
+      <p className={s.yourAccount}>Your Account</p>
 
       <div className={s.user}>
         <div>
@@ -54,14 +69,14 @@ export default function YourAccount() {
           variant="outlined"
           startIcon={<ShoppingBagIcon />}
           className={s.button}
-          onClick={()=> console.log('orders')}
+          onClick={() => console.log("orders")}
         >
           Your Orders
         </Button>
         <Button
           variant="outlined"
           startIcon={<FavoriteIcon />}
-          onClick={()=> console.log('favs')}
+          onClick={() => console.log("favs")}
         >
           Your Favs
         </Button>
@@ -70,23 +85,23 @@ export default function YourAccount() {
         <Button
           variant="outlined"
           startIcon={<HomeRepairServiceIcon />}
-          onClick={()=> console.log('service')}
+          onClick={() => console.log("service")}
         >
           Post Service
         </Button>
         <Button
           variant="outlined"
-          startIcon={<HomeRepairServiceIcon />}
-          onClick={()=> console.log('service')}
+          startIcon={<VisibilityIcon />}
+          onClick={() => console.log("service")}
         >
           Your Services
         </Button>
         <Button
           variant="outlined"
-          startIcon={<HomeRepairServiceIcon />}
-          onClick={()=> console.log('profile data')}
+          startIcon={<DataSaverOffIcon />}
+          onClick={() => console.log("profile data")}
         >
-          Change Profile Data
+          Change Your Data
         </Button>
       </div>
     </div>
