681a122b8142e115c8e802aff0c1c434fd406a8b
Fedex159 <fede.gallina94@gmail.com>
2021-10-27 18:27:27 -0300

    api/index: load groups and category

--- api/index.js
+++ api/index.js
@@ -20,18 +20,27 @@
 const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
 const { json } = require("./mock/dbJson");
-const { services } = require("./mock/servicesJson");
+const { linkAllGroups } = require("./mock/categories");
+const { groups } = require("./mock/groups");
 const { users } = require("./mock/usersJson");
-const { Service, Category, Users } = require("./src/db");
+const { Service, Users, Group } = require("./src/db");
 const { ENV_VARIABLE } = process.env;
 
 conn.sync({ force: Boolean(Number(ENV_VARIABLE)) }).then(() => {
   server.listen(3001, () => {
-    Service.bulkCreate(json).then(() => console.log("Datos Cargados"));
-    Category.bulkCreate(services).then(() => console.log("Servicios Cargados"));
+    Service.bulkCreate(json).then(() => console.log("Servicios Cargados"));
     Users.bulkCreate(users, { individualHooks: true }).then(() =>
       console.log("Users Cargados")
     );
+    Group.bulkCreate(groups)
+      .then(() => {
+        console.log("Grupos cargados");
+        Promise.resolve(linkAllGroups()).then(() =>
+          console.log("Categorias cargadas")
+        );
+      })
+      .catch((e) => console.log(e));
+
     console.log("%s listening at 3001"); // eslint-disable-line no-console
   });
 });
