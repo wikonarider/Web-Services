ddd08af7b7b0b97f3525bca2d8da1fc80738dcf8
Fedex159 <fede.gallina94@gmail.com>
2021-10-27 14:55:45 -0300

    api/db: fix hash password when update user

--- api/src/db.js
+++ api/src/db.js
@@ -1,9 +1,9 @@
-require('dotenv').config();
-const { Sequelize } = require('sequelize');
-const fs = require('fs');
-const path = require('path');
+require("dotenv").config();
+const { Sequelize } = require("sequelize");
+const fs = require("fs");
+const path = require("path");
 const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env;
-const bcrypt = require('bcrypt');
+const bcrypt = require("bcrypt");
 
 const sequelize = new Sequelize(
   `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`,
@@ -17,13 +17,13 @@ const basename = path.basename(__filename);
 const modelDefiners = [];
 
 // Leemos todos los archivos de la carpeta Models, los requerimos y agregamos al arreglo modelDefiners
-fs.readdirSync(path.join(__dirname, '/models'))
+fs.readdirSync(path.join(__dirname, "/models"))
   .filter(
     (file) =>
-      file.indexOf('.') !== 0 && file !== basename && file.slice(-3) === '.js'
+      file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
   )
   .forEach((file) => {
-    modelDefiners.push(require(path.join(__dirname, '/models', file)));
+    modelDefiners.push(require(path.join(__dirname, "/models", file)));
   });
 
 // Injectamos la conexion (sequelize) a todos los modelos
@@ -41,11 +41,11 @@ sequelize.models = Object.fromEntries(capsEntries);
 const { Service, Users, Qualification, Category } = sequelize.models;
 
 // Aca vendrian las relaciones
-Service.belongsToMany(Users, { through: 'services_users_bought' });
-Users.belongsToMany(Service, { through: 'services_users_bought' });
+Service.belongsToMany(Users, { through: "services_users_bought" });
+Users.belongsToMany(Service, { through: "services_users_bought" });
 
-Service.belongsToMany(Users, { through: 'services_users_favourites' });
-Users.belongsToMany(Service, { through: 'services_users_favourites' });
+Service.belongsToMany(Users, { through: "services_users_favourites" });
+Users.belongsToMany(Service, { through: "services_users_favourites" });
 
 Users.hasMany(Service);
 Service.belongsTo(Users);
@@ -64,6 +64,12 @@ Qualification.belongsTo(Users);
 // hooks users
 // Encripta la contraseña antes de crear el usuario
 Users.beforeCreate(async function (user) {
+  const salt = await bcrypt.genSalt(10);
+  user.password = await bcrypt.hash(user.password, salt);
+});
+
+// Encripta la contraseña cuando el usuario la cambia
+Users.beforeUpdate(async function (user) {
   const salt = await bcrypt.genSalt(10);
   user.password = await bcrypt.hash(user.password, salt);
 });
