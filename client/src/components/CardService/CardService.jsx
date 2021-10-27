015704acde653181ab3ea9ec6659f378d4f1111a
Fedex159 <fede.gallina94@gmail.com>
2021-10-27 20:08:52 -0300

    front: remove http://localhost and fix warnings

--- client/src/components/CardService/CardService.jsx
+++ client/src/components/CardService/CardService.jsx
@@ -6,7 +6,7 @@ import CardActions from "@mui/material/CardActions";
 import IconButton from "@mui/material/IconButton";
 import FavoriteIcon from "@mui/icons-material/Favorite";
 import ShareIcon from "@mui/icons-material/Share";
-import { Typography , CardActionArea } from "@mui/material";
+import { Typography, CardActionArea } from "@mui/material";
 import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
 import Rating from "@mui/material/Rating";
 import { Link } from "react-router-dom";
@@ -14,39 +14,33 @@ import { Link } from "react-router-dom";
 const IMG_TEMPLATE =
   "https://codyhouse.co/demo/squeezebox-portfolio-template/img/img.png";
 
-
 function CardService({ service }) {
   const { title, img, price, id } = service;
   const rating = 5;
   return (
-    <Card sx={{ width: 345, height: 420, textDecoration: 'none' }}>
-      
+    <Card sx={{ width: 345, height: 420, textDecoration: "none" }}>
       <CardActionArea component={Link} to={`/services/${id}`}>
-      <CardHeader 
-      title={title} 
-      sx={{ pb: "0", height: "64px" }} 
-      />
-      <Rating
-        name="read-only"
-        value={rating}
-        precision={0.5}
-        readOnly
-        sx={{ p: "8px" }}
-      />
+        <CardHeader title={title} sx={{ pb: "0", height: "64px" }} />
+        <Rating
+          name="read-only"
+          value={rating}
+          precision={0.5}
+          readOnly
+          sx={{ p: "8px" }}
+        />
 
-      
         <CardMedia
-        component="img"
-        height="194"
-        image={img ? img : IMG_TEMPLATE}
-        alt={title}
-        sx={{ objectFit: "cover" }}
-      />
-      <Typography variant="h5" component="div" sx={{ p: "5px" }}>
-        {`$${price ? price : 0}`}
-      </Typography>
+          component="img"
+          height="194"
+          image={img ? img : IMG_TEMPLATE}
+          alt={title}
+          sx={{ objectFit: "cover" }}
+        />
+        <Typography variant="h5" component="div" sx={{ p: "5px" }}>
+          {`$${price ? price : 0}`}
+        </Typography>
       </CardActionArea>
-      
+
       <CardActions disableSpacing>
         <IconButton onClick={() => {}} aria-label="add to favorites">
           <FavoriteIcon sx={{}} />
--- client/src/components/CreateService/CreateService.jsx
+++ client/src/components/CreateService/CreateService.jsx
@@ -1,228 +1,184 @@
-import { React, useState, useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
+import { React, useState } from "react";
+import { useDispatch } from "react-redux";
 import { TextField, Button } from "@mui/material";
-import s from './CreateService.module.css'
-import { createService } from '../../redux/actions';
-import axios from 'axios'
-import ModalService from './ModalService'
-
-
-
-
+import s from "./CreateService.module.css";
+import { createService } from "../../redux/actions";
+import axios from "axios";
+import ModalService from "./ModalService";
 
 export default function CreateService() {
+  const dispatch = useDispatch();
 
-    const dispatch = useDispatch()
+  const [errors, setErrors] = useState({});
+  const [modal, setModal] = useState(false);
+  const [inputs, setInputs] = useState({
+    title: "",
+    description: "",
+    price: "",
+    userName: "frankera1312",
+    img: "",
+  });
 
+  function isNumber(price) {
+    return /^[+-]?\d*\.?\d+(?:[Ee][+-]?\d+)?$/.test(price);
+  }
 
-    const [errors, setErrors] = useState({})
-    const [modal, setModal] = useState(false);
-    const [inputs, setInputs] = useState({
-        title: '',
-        description: '',
-        price: '',
-        userName: 'frankera1312',
-        img: ''
-    })
+  function errorsValidate(inputs) {
+    let errors = {};
 
-
-
-
-
-
-
-    function isNumber(price) {
-        return (
-            /^[\+\-]?\d*\.?\d+(?:[Ee][\+\-]?\d+)?$/.test(price)
-        );
+    if (!inputs.title) {
+      errors.title = "Title is required";
+    } else if (!inputs.description) {
+      errors.description = "Description is required";
+    } else if (!inputs.price) {
+      errors.price = "Price is required";
+    } else if (!isNumber(inputs.price)) {
+      errors.price = "Price must to be a number";
     }
 
-    function errorsValidate(inputs) {
-        let errors = {}
+    return errors;
+  }
 
-        if (!inputs.title) {
-            errors.title = 'Title is required'
-        }
+  function handleInput(e) {
+    setInputs((prev) => {
+      const input = {
+        ...prev,
+        [e.target.name]: e.target.value,
+      };
+      // console.log(inputs)
 
-        else if (!inputs.description) {
-            errors.description = 'Description is required'
-        }
-
-
-        else if (!inputs.price) {
-            errors.price = 'Price is required'
-        }
-        else if (!isNumber(inputs.price)) {
-            errors.price = "Price must to be a number"
-        }
-
-
-        return errors
-    }
-
-
-
-    function handleInput(e) {
-        setInputs((prev) => {
-
-            const input = {
-                ...prev,
-                [e.target.name]: e.target.value,
-            };
-            // console.log(inputs)
-
-            setErrors(() => {
-                return errorsValidate({
-                    ...inputs,
-                    [e.target.name]: e.target.value
-                });
-            });
-
-            // console.log('errors', errors)
-
-
-            return input;
+      setErrors(() => {
+        return errorsValidate({
+          ...inputs,
+          [e.target.name]: e.target.value,
         });
+      });
+
+      // console.log('errors', errors)
+
+      return input;
+    });
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault(e);
+    if (inputs.title && inputs.img && inputs.description && inputs.price) {
+      setModal(true);
+      dispatch(createService(inputs));
+      setInputs({
+        title: "",
+        description: "",
+        price: "",
+        userName: "frankera1312",
+        img: "",
+      });
+    } else {
+      alert("Faltan campos por completar");
+    }
+  }
+
+  function loadImg(files) {
+    const reader = new FileReader();
+    reader.onload = function () {
+      let imgDiv = document.querySelector("#imgBox");
+      imgDiv.src = reader.result; //Básicamente lo que hago acá es
+      //convertir la img en una URL para poder mandarla
     };
+    reader.readAsDataURL(files);
 
-    function handleSubmit(e) {
-        e.preventDefault(e)
-        if (inputs.title && inputs.img && inputs.description && inputs.price) {
-            setModal(true);
-            dispatch(createService(inputs))
-            setInputs({
-                title: '',
-                description: '',
-                price: '',
-                userName: 'frankera1312',
-                img: ''
+    const formData = new FormData();
+    formData.append("file", files);
+    formData.append("upload_preset", "za6qmkus");
+    axios
+      .post("https://api.cloudinary.com/v1_1/dd9t6masq/auto/upload", formData)
+      .then((response) =>
+        setInputs({ ...inputs, img: response.data.secure_url })
+      );
+    console.log("url", inputs.img);
+    //hacer cuenta cloudinay
+  }
 
-            })
-        }
-        else {
-            alert('Faltan campos por completar')
-        }
+  return (
+    <div className={s.container}>
+      <form onSubmit={(e) => handleSubmit(e)} className={s.formWrapper}>
+        <div className={s.inputsContainer}>
+          <TextField
+            name="title"
+            placeholder="Title"
+            value={inputs.title}
+            onChange={handleInput}
+            error={errors.title ? true : false}
+            helperText={errors.title}
+            label="Title"
+            variant="outlined"
+            required
+            fullWidth
+          ></TextField>
+        </div>
 
+        <div className={s.inputsContainer}>
+          <TextField
+            name="description"
+            placeholder="Description"
+            value={inputs.description}
+            onChange={handleInput}
+            error={errors.description ? true : false}
+            helperText={errors.description}
+            label="Description"
+            variant="outlined"
+            required
+            fullWidth
+          ></TextField>
+        </div>
 
-    }
+        <div className={s.inputsContainer}>
+          <TextField
+            name="price"
+            placeholder="Price"
+            value={inputs.price}
+            onChange={handleInput}
+            error={errors.price ? true : false}
+            helperText={errors.price}
+            label="Price"
+            variant="outlined"
+            required
+            fullWidth
+          ></TextField>
+        </div>
 
-    function loadImg(files) {
-        const reader = new FileReader();
-        reader.onload = function () {
-            let imgDiv = document.querySelector("#imgBox")
-            imgDiv.src = reader.result;                            //Básicamente lo que hago acá es
-                                                                  //convertir la img en una URL para poder mandarla  
-        }
-        reader.readAsDataURL(files);
+        <div className={s.inputsContainer}>
+          <TextField
+            name="img"
+            type="file"
+            fullWidth
+            onChange={(e) => loadImg(e.target.files[0])}
+          />
+        </div>
 
-        const formData = new FormData();
-        formData.append('file', files);
-        formData.append("upload_preset", "za6qmkus");
-        axios.post(
-            "https://api.cloudinary.com/v1_1/dd9t6masq/auto/upload",
-            formData
-        ).then((response) =>
-            setInputs({ ...inputs, img: response.data.secure_url }))
-        console.log('url', inputs.img)
-        //hacer cuenta cloudinay
-    }
+        <div className={s.imgContainer}>
+          <img id="imgBox" src="hola" alt=""></img>
+        </div>
 
-    return (
-        <div className={s.container}>
-            <form onSubmit={(e) => handleSubmit(e)} className={s.formWrapper}>
+        {!errors.title && !errors.description && !errors.price ? (
+          <div className={s.submitButton}>
+            <Button type="submit" variant="contained">
+              Create Service
+            </Button>
+          </div>
+        ) : (
+          <div className={s.submitButton}>
+            <Button disabled type="submit" variant="contained">
+              Create Service
+            </Button>
+          </div>
+        )}
+      </form>
 
-                <div className={s.inputsContainer}>
-                    <TextField
-                        name='title'
-                        placeholder='Title'
-                        value={inputs.title}
-                        onChange={handleInput}
-                        error={errors.title ? true : false}
-                        helperText={errors.title}
-                        label="Title"
-                        variant="outlined"
-                        required
-                        fullWidth>
-                    </TextField>
-                </div>
-
-                <div className={s.inputsContainer}>
-                    <TextField
-                        name='description'
-                        placeholder='Description'
-                        value={inputs.description}
-                        onChange={handleInput}
-                        error={errors.description ? true : false}
-                        helperText={errors.description}
-                        label="Description"
-                        variant="outlined"
-                        required
-                        fullWidth>
-                    </TextField>
-                </div>
-
-                <div className={s.inputsContainer}>
-                    <TextField
-                        name='price'
-                        placeholder='Price'
-                        value={inputs.price}
-                        onChange={handleInput}
-                        error={errors.price ? true : false}
-                        helperText={errors.price}
-                        label="Price"
-                        variant="outlined"
-                        required
-                        fullWidth>
-                    </TextField>
-                </div>
-
-                <div className={s.inputsContainer}>
-                    <TextField
-                        name="img"
-                        type="file"
-                        fullWidth
-                        onChange={(e => loadImg(e.target.files[0]))} />
-                </div>
-
-                <div className={s.imgContainer}>
-                    <img
-                        id='imgBox'
-                        src='hola'
-                        alt=''>
-                    </img>
-                </div>
-
-
-                {!errors.title && !errors.description && !errors.price ?
-                    <div className={s.submitButton}>
-                        <Button
-                            type="submit"
-                            variant="contained">
-                            Create Service
-                        </Button>
-
-                    </div> :
-
-                    <div className={s.submitButton}>
-                        <Button
-                            disabled
-                            type="submit"
-                            variant="contained">
-                            Create Service
-                        </Button>
-                    </div>
-                }
-
-            </form>
-
-            <ModalService
+      <ModalService
         modal={modal}
         setModal={setModal}
         message={"¡Service created successfully!"}
       />
-        </div>
-    )
+    </div>
+  );
 }
-
-
-
