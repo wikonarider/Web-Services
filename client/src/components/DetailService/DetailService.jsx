719b9fd10b95425454e1fe3cde846f8ca90a4a75
stobar93 <sebastiantobarquintero@gmail.com>
2021-10-26 22:07:08 -0500

    Close button working

--- client/src/components/DetailService/DetailService.jsx
+++ client/src/components/DetailService/DetailService.jsx
@@ -1,8 +1,9 @@
 import React , {useEffect, useState} from "react";
+import { useHistory } from "react-router-dom";
 import axios from 'axios';
 import { Grid , Box, Card, CardMedia, CardContent, Typography, Rating,
           CardActions, IconButton} from '@mui/material';
-import {AddShoppingCart, Favorite, Share, ShareOutlined} from "@mui/icons-material";
+import {AddShoppingCart, Favorite, Share, Close} from "@mui/icons-material";
 
 
 
@@ -10,11 +11,17 @@ export default function DetailService({id}){
   
   let [service, setService] = useState({})
 
+  let history = useHistory();
+
   //componentDidMount para traer la información del servicio por id
   useEffect(()=>{ 
     axios(`http://localhost:3001/services/${id}`)
     .then(response=>setService(response.data))
 }, [] ); 
+
+  const handleClose =()=>{
+    history.goBack()
+  }
 
   const IMG_TEMPLATE =
   "https://codyhouse.co/demo/squeezebox-portfolio-template/img/img.png";
@@ -38,9 +45,27 @@ export default function DetailService({id}){
             />
         </Box> 
 
+        
         <Box gridColumn="span 4" m={2} p={2} border="solid 1px lightgrey">
+          <Box gridColumn="span 12" display="flex" flexDirection="row" justifyContent="space-between">
+            <Box gridColumn="span 6">
+              <IconButton onClick={() => {}} aria-label="add to favorites">
+                <Favorite sx={{}} />
+              </IconButton>
+              <IconButton aria-label="share">
+                <Share />
+              </IconButton>
+            </Box>
+
+            <Box gridColumn="span 6">
+            <IconButton onClick={()=>handleClose()}>
+                <Close />
+            </IconButton>
+            </Box>
+          </Box>
+
           <Box gridColumn="span 12" display="flex" flexDirection="row" flexWrap="wrap" alignContent="start">
-              <Typography variant="h5"> {title} </Typography>
+              <Typography variant="h5" sx={{width: "100%", textAlign: "left"}}> {title} </Typography>
               <Rating
                 name="read-only"
                 value={rating}
@@ -62,25 +87,15 @@ export default function DetailService({id}){
               >
                 <AddShoppingCart />
               </IconButton>
-              <IconButton onClick={() => {}} aria-label="add to favorites">
-                <Favorite sx={{}} />
-              </IconButton>
-              <IconButton aria-label="share">
-                <Share />
-              </IconButton>
+              
             </CardActions>
           </Box>
 
           <Box gridColumn="span 12">
-            <Typography variant="subtitle1" component="div" sx={{textAlign: "left"}}> {description} </Typography>
+            <Typography variant="subtitle1" component="div" sx={{textAlign: "left"}}> Description: </Typography>
+            <Typography variant="subtitle2" component="div" sx={{textAlign: "left"}}> {description} </Typography>
           </Box>
         </Box>
-        
-        
-        
-      </Box>
-      
-      
-        
+      </Box>  
     )
 }
