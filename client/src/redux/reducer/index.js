cd77eef4d75b394badcd594dbfa3b30efb8235c4
Facundo <86330512+Facupelli@users.noreply.github.com>
2021-10-27 16:18:12 -0300

    fix(client)import type (#49)

--- client/src/redux/reducer/index.js
+++ client/src/redux/reducer/index.js
@@ -1,30 +1,28 @@
+import { type } from "../actions/variables";
 
 const initialState = {
   services: [],
   users: [],
 };
 
-
 const rootReducer = (state = initialState, action) => {
   const { payload } = action;
   switch (action.type) {
-//usar importacion type que incluye las constantes para facilitarte,agregar constantes si es necesario en variables.js ""
-  
+    //usar importacion type que incluye las constantes para facilitarte,agregar constantes si es necesario en variables.js ""
+
     case type.GET_SERVICES:
-      console.log('Payload en reducer',action.payload)
-      console.log('globalState de servicios',state.services)
+      console.log("Payload en reducer", action.payload);
+      console.log("globalState de servicios", state.services);
       return {
         ...state,
         services: payload,
       };
 
-     case type.CREATE_SERVICE:
-        return { ...state}
+    case type.CREATE_SERVICE:
+      return { ...state };
 
-     case type.GET_USERS:
-        return { ...state,
-        users: action.payload}
-
+    case type.GET_USERS:
+      return { ...state, users: action.payload };
 
     default:
       return state;
