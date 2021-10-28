883c15ad9a6d3cc52f8ee99dba6c47b05f7a9205
Fedex159 <fede.gallina94@gmail.com>
2021-10-28 15:33:50 -0300

    api/models: models created

---
+++ api/src/models/Services_Provinces.js
@@ -0,0 +1,11 @@
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+  sequelize.define(
+    "services_provinces",
+    {},
+    {
+      timestamps: false,
+    }
+  );
+};
