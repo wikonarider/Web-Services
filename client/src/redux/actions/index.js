6c17704fa5ee447d23f0776f8df0e1bcb0485189
Micaela Montero <wikonarider@gmail.com>
2021-10-28 17:02:09 -0300

    add order in action getServices (#70)

--- client/src/redux/actions/index.js
+++ client/src/redux/actions/index.js
@@ -1,12 +1,12 @@
-import { type } from './variables';
-import axios from 'axios';
+import { type } from "./variables";
+import axios from "axios";
 //_____________________________________________________________________________________actions service
 // usar axios("/route"), no es necesario http://localhost:3001, ya
 // esta configurado en el archivo index.js
-export function getServices(title) {
+export function getServices(title, order) {
   return async function (dispatch) {
     try {
-      var json = await axios(`/services?title=${title}`);
+      var json = await axios(`/services?title=${title}&order=${order}`);
       return dispatch({
         type: type.GET_SERVICES,
         payload: json.data,
@@ -67,7 +67,7 @@ export function deleteService(id) {
 export function postUser(data) {
   return async () => {
     try {
-      return await axios.post('/users/', data);
+      return await axios.post("/users/", data);
     } catch (err) {
       return new Error(err);
     }
@@ -77,7 +77,7 @@ export function postUser(data) {
 export function putUser(newData) {
   return async () => {
     try {
-      return await axios.post('/users/', newData);
+      return await axios.post("/users/", newData);
     } catch (err) {
       return new Error(err);
     }
@@ -109,10 +109,8 @@ export function createService(body) {
   return async function (dispatch) {
     var json = await axios.post(`/services`, body);
     return dispatch({
-      type: 'CREATE_SERVICE',
+      type: "CREATE_SERVICE",
       payload: json.data,
     });
   };
 }
-
-
