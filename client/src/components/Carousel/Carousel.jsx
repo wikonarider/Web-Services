ae9d660a82bfe903ea0a1d11e8663996529199bf
Facundo <86330512+Facupelli@users.noreply.github.com>
2021-10-28 10:37:39 -0300

    added carousel properties fro customize (#60)

--- client/src/components/Carousel/Carousel.jsx
+++ client/src/components/Carousel/Carousel.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Carousel from "react-material-ui-carousel";
 import {
   Button,
@@ -39,7 +39,6 @@ const featured = [
     img: "https://www.ciudaddelosangeles.com/wp-content/uploads/2016/03/paseador-de-perros.jpg",
     price: 15.3,
   },
-
   {
     title: "musical performance drag queen",
     description: "te hago tu señor show",
@@ -50,6 +49,24 @@ const featured = [
     title: "prfesorx de yoga",
     description: "i love little dogs",
     img: "https://www.yogaenred.com/wp-content/uploads/2020/04/Profesora-yoga-1-605x403.jpg",
+    price: 28.3,
+  },
+  {
+    title: "Plomero",
+    description: "te plomeo todo",
+    img: "https://procrew.b-cdn.net/wp-content/uploads/2020/06/plumber-at-work-in-a-bathroom-1024x683-1-768x512.jpg",
+    price: 15.3,
+  },
+  {
+    title: "Fletes",
+    description: "fletes a la velocidad del rayo mqueen cuchau",
+    img: "https://fotos.perfil.com/2020/05/16/trim/1280/720/20200516comerciociudadfletesprotocolojuanobregong-957557.jpg",
+    price: 19.3,
+  },
+  {
+    title: "Reparacion pc",
+    description: "te dejo la pc gamer redy",
+    img: "https://www.cetae.com.ar/wp-content/uploads/2018/02/reppc.jpg",
     price: 28.3,
   },
 ];
@@ -72,10 +89,30 @@ const useStyles = makeStyles({
 });
 
 export default function Carrousel(props) {
+  const [state, setState] = useState({
+    autoPlay: true,
+    animation: "slide",
+    indicators: true,
+    timeout: 600,
+    navButtonsAlwaysVisible: false,
+    navButtonsAlwaysInvisible: false,
+    cycleNavigation: true,
+  });
+
   return (
-    <Carousel className={s.carousel}>
+    <Carousel
+      className={s.carousel}
+      autoPlay={state.autoPlay}
+      animation={state.animation}
+      indicators={state.indicators}
+      timeout={state.timeout}
+      cycleNavigation={state.cycleNavigation}
+      navButtonsAlwaysVisible={state.navButtonsAlwaysVisible}
+      navButtonsAlwaysInvisible={state.navButtonsAlwaysInvisible}
+    >
       {Items(0, 3)}
       {Items(3, 6)}
+      {Items(6, 9)}
     </Carousel>
   );
 }
