eb253aa3d62c197c2d31618a6073131fb5ac52f0
Fedex159 <fede.gallina94@gmail.com>
2021-10-28 16:37:37 -0300

    api/mockup: cities and provincies created

---
+++ api/mock/cities.js
@@ -0,0 +1,20 @@
+const axios = require("axios");
+
+async function loadCities() {
+  const response = await axios(
+    "https://infra.datos.gob.ar/catalog/modernizacion/dataset/7/distribution/7.3/download/departamentos.json"
+  );
+  let bulk = [];
+  response.data.departamentos.forEach((element) => {
+    bulk.push({
+      id: Number(element.id),
+      name: element.nombre,
+      provinceId: Number(element.provincia.id),
+    });
+  });
+  return bulk;
+}
+
+module.exports = {
+  loadCities,
+};
