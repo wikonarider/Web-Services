f39e5f3afa04ec224f1de42849c2040aaa1aa0d6
Fedex159 <fede.gallina94@gmail.com>
2021-10-28 10:58:18 -0300

    api/utils: added validServices

---
+++ api/src/utils/validServices.js
@@ -0,0 +1,61 @@
+const { Category } = require("../db");
+
+function validateUrl(str) {
+  const pattern = new RegExp(
+    "^(https?:\\/\\/)?" + // protocol
+      "((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|" + // domain name
+      "((\\d{1,3}\\.){3}\\d{1,3}))" + // OR ip (v4) address
+      "(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*" + // port and path
+      "(\\?[;&a-z\\d%_.~+=-]*)?" + // query string
+      "(\\#[-a-z\\d_]*)?$",
+    "i"
+  ); // fragment locator
+  return !!pattern.test(str);
+}
+
+async function validateCategoryId(id) {
+  const category = await Category.findByPk(id);
+  return category ? true : false;
+}
+
+async function validateServices(service) {
+  let errors = {};
+
+  if (typeof service.img !== "string") {
+    errors.img = "It has to be of type string";
+  } else if (!validateUrl(service.img)) {
+    errors.img = "It has to be a valid url";
+  }
+
+  if (typeof service.description !== "string") {
+    errors.description = "It has to be of type string";
+  } else if (!service.description.length) {
+    errors.description = "Can't be empty";
+  }
+
+  if (typeof service.price !== "number") {
+    errors.price = "It must be a number";
+  } else if (service.price < 100) {
+    errors.price = "Has to be greater than 100";
+  }
+
+  if (typeof service.title !== "string") {
+    errors.title = "It has to be of type string";
+  } else if (!service.title.length) {
+    errors.title = "Can't be empty";
+  } else if (service.title.length > 40) {
+    errors.title = "It cannot have more than 40 characters";
+  }
+
+  if (typeof service.categoryId !== "number") {
+    errors.categoryId = "It must be a number";
+  } else if (!(await validateCategoryId(service.categoryId))) {
+    errors.categoryId = "It has to be a valid category id";
+  }
+
+  return errors;
+}
+
+module.exports = {
+  validateServices,
+};
