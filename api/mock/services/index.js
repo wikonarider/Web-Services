39be9420840eaeb4a134a6ad1a691875947aebc5
Micaela Montero <wikonarider@gmail.com>
2021-10-29 18:16:43 -0300

    add mock/transport (#99)

--- api/mock/services/index.js
+++ api/mock/services/index.js
@@ -1,9 +1,11 @@
-const { Service } = require('../../src/db');
-const { lessons } = require('./lessons');
-const { repair } = require('./repairs');
+
+const { Service } = require("../../src/db");
+const { lessons } = require("./lessons");
+const { repair } = require("./repairs");
+const { transport } = require("./transport");
 const {eventsOrganization} = requite('./eventsOrganization')
 
-const bulk = [...lessons, ...repair, ...eventsOrganization];
+const bulk = [...lessons, ...repair, ...transport, ...eventsOrganization];
 
 function loadServices() {
   return Service.bulkCreate(bulk);
---
+++ api/mock/services/transport.js
@@ -0,0 +1,119 @@
+const freight = [
+  {
+    title: "Fletes, Repartos, Distribuciones, Trasbordo",
+    description:
+      "Contamos con un Vehículo en Santa Fe, apto para transportar hasta 2 tn, y 5 palet, cerrado con lona. Servicios a empresas y particulares. Consulte su presupuesto.",
+    img: "https://apollo-virginia.akamaized.net/v1/files/2jojqo62heib3-AR/image;s=780x0;q=60",
+    price: 1500,
+    categoryId: 27,
+    userId: "4e21929a-36ad-11ec-8d3d-0242ad130003",
+  },
+  {
+    title: "Fletes",
+    description:
+      "Fletes y envíos de paquetería. Seriedad y Responsabilidad. San Luis.",
+    img: "https://apollo-virginia.akamaized.net/v1/files/bysymkgnzl88-AR/image;s=780x0;q=60",
+    price: 1000,
+    categoryId: 27,
+    userId: "4e218b24-36ad-11ec-8d3d-0242ac130003",
+  },
+  {
+    title: "Remolque de Motos",
+    description:
+      "Auxilio de Motos. Traslado de cuatriciclos. Trailer amplio para motos y cuatriciclos grandes. En Mar del Plata. También traslados al interior del País.",
+    img: "https://apollo-virginia.akamaized.net/v1/files/bysymkgnzl88-AR/image;s=780x0;q=60",
+    price: 1200,
+    categoryId: 27,
+    userId: "4e218c00-36ad-11ec-8d3d-0242ac130003",
+  },
+];
+
+const transport = [
+  {
+    title: "Servicio de limusinas",
+    description: "Servicio de limusinas. Por consultas enviar mensaje.",
+    img: "https://apollo-virginia.akamaized.net/v1/files/1oz2dh68vdvf3-AR/image;s=780x0;q=60",
+    price: 3000,
+    categoryId: 28,
+    userId: "4e219812-36ad-11ec-8d3d-0242ac139003",
+  },
+  {
+    title: "Te llevo al uruguay : Remis ",
+    description:
+      "Remis : Te llevó al uruguay para 4 personas maximo grand baul . En un Renault Fluence Gt2 sport. Al iniciar el viaje pago adelantado en efectivo.",
+    img: "https://apollo-virginia.akamaized.net/v1/files/wp70a6zmlk19-AR/image;s=780x0;q=60",
+    price: 30000,
+    categoryId: 27,
+    userId: "4e21899e-36ad-11ec-8d3d-0242ac130003",
+  },
+  {
+    title: "Traslado de mascotas a todo el país.",
+    description:
+      "Llevamos a tú mascota a cualquier punto del país.Servicio puerta a puerta ( peajes incluidos). Camioneta habilitada únicamente para el traslado de su perro/gato u otra mascota.Servicio seguro, cálido, puntual y efectivo.",
+    img: "https://apollo-virginia.akamaized.net/v1/files/3rixay00r46j1-AR/image;s=780x0;q=60",
+    price: 30,
+    categoryId: 27,
+    userId: "4e218f98-36ad-11ec-8d3d-0242ac130003",
+  },
+];
+const moving = [
+  {
+    title: "FLETES Y MUDANZAS CIUDAD Y LARGA DISTANCIA",
+    description:
+      "Se realizan fletes y mudanzas..ciudad ..zona y larga distancia.aceptamos mercado de pago..se extiende factura y se asegura la carga. Consulte presupuesto con o sin ayudante. Mudanza por hora. Larga distancia por km.",
+    img: "https://apollo-virginia.akamaized.net/v1/files/1oz2dh68vdvf3-AR/image;s=780x0;q=60",
+    price: 500,
+    categoryId: 28,
+    userId: "4e21929a-36ad-11ec-8d3d-0242ac170003",
+  },
+  {
+    title: "MINIFLETES ECONOMICOS ",
+    description:
+      "Seguridad y puntualidad. Fletes, minifletes repartos y mudanzas. Desde $ 900",
+    img: "https://apollo-virginia.akamaized.net/v1/files/fsit0amipmit2-AR/image;s=780x0;q=60",
+    price: 1200,
+    categoryId: 28,
+    userId: "4e21929a-36ad-11ec-8d3d-0242ad130003",
+  },
+  {
+    title: "Flete o pequeña mudanza a Mar Del Plata.",
+    description:
+      "Soy de Mar Del Plata, realizo Fletes a y desde Mar Del Plata.",
+    img: "https://apollo-virginia.akamaized.net/v1/files/fevnodzm2n1d-AR/image;s=780x0;q=60",
+    price: 28000,
+    categoryId: 28,
+    userId: "4e219510-36ad-11ec-8d3d-0242ac110003",
+  },
+];
+
+const other = [
+  {
+    title: "SERVICIO DE HIDROGRUA",
+    description:
+      "Hidrogrua Amco Veba 936... 6900kgs al pie. 4 extensiones hidraulicas.",
+    img: "https://apollo-virginia.akamaized.net/v1/files/epgydipntor01-AR/image;s=780x0;q=60",
+    price: 4000,
+    categoryId: 29,
+    userId: "4e21899e-36ad-11ec-8d3d-0242ac130003",
+  },
+  {
+    title: "Trailer gastronómico o bien para casa rodante",
+    description:
+      "Se vende trailer completo o vacío.. sirve como foodtruck o para casa rodante..",
+    img: "https://apollo-virginia.akamaized.net/v1/files/d8dnlhyid9ps2-AR/image;s=780x0;q=60",
+    price: 35000,
+    categoryId: 29,
+    userId: "4e219812-36ad-11ec-8d3d-0242ac139003",
+  },
+  {
+    title: "Habilitación de taxi",
+    description: "Habilitación de taxi La Plata",
+    img: "https://apollo-virginia.akamaized.net/v1/files/nz1js4kyqs1a1-AR/image;s=780x0;q=60",
+    price: 1111111,
+    categoryId: 29,
+    userId: "4e219812-36ad-11ec-8d3d-0242ac139003",
+  },
+];
+module.exports = {
+  transport,
+};
