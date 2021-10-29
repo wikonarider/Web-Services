93e10cef638223990d97a4514b549ba367c03220
Jonatan Segovia <jsegovia.ush@gmail.com>
2021-10-29 18:48:17 -0300

    bug/events reparado (#101)

--- api/mock/services/index.js
+++ api/mock/services/index.js
@@ -1,9 +1,8 @@
-
-const { Service } = require("../../src/db");
-const { lessons } = require("./lessons");
-const { repair } = require("./repairs");
-const { transport } = require("./transport");
-const {eventsOrganization} = requite('./eventsOrganization')
+const { Service } = require('../../src/db');
+const { lessons } = require('./lessons');
+const { repair } = require('./repairs');
+const { transport } = require('./transport');
+const { eventsOrganization } = require('./eventsOrganization');
 
 const bulk = [...lessons, ...repair, ...transport, ...eventsOrganization];
 
