7d27994659479aba0931277d0c2a651a6e076a99
Fedex159 <fede.gallina94@gmail.com>
2021-10-29 09:30:26 -0300

    api/mock: lessons mock created

---
+++ api/mock/services/lessons.js
@@ -0,0 +1,414 @@
+const danzas = [
+  {
+    title: "clases de rock and roll y americanos",
+    description:
+      "queres aprender rock and roll y americano rock y classicos en quilmes centro.son clases personalizadas de 1 hora x persona..vos elegis dia y hora...i",
+    img: "https://apollo-virginia.akamaized.net/v1/files/gpvje9jg0hzx1-AR/image;s=1080x1080",
+    price: 500,
+    categoryId: 1,
+    userId: "4e21929a-36ad-11ec-8d3d-0242ad130003",
+  },
+  {
+    title: "Clases Danza Árabe individuales o dúos",
+    description:
+      "Clases de Danzas Arabes en Olivos, zona comercial, Profesora Nayla Nader , actividad con protocolo, las clases son individuales o en dúos, vos elegís, consultas al whats app.",
+    img: "https://www.clubfuturama.com/appManager/recursos/img/1/stock/DAN55d3ab055f169_movHNormal.jpg",
+    price: 1000,
+    categoryId: 1,
+    userId: "4e21929a-36ad-11ec-8d3d-0242ad130003",
+  },
+  {
+    title: "ACRODANCE FUSION NIÑAS PEQUEÑAS",
+    description: `Acrodance fusion con la Gimnasia artistica y la danza, niñas pequeñas. Protocolo, clases presenciales, grupos reducidos.
+
+    Danza clasica. Gimnasia artistica, coreografia, tambien clases de pole dance niñas,
+    
+    clases para adultos de partenaire acrobatico - Contemporaneo - Hip hop - grupo familiar con protocolo grupo reducido
+    
+    INFO x telefono
+    
+    Balbin y congreso`,
+    img: "https://apollo-virginia.akamaized.net/v1/files/xx9ee23u05u53-AR/image;s=780x0;q=60",
+    price: 2900,
+    categoryId: 1,
+    userId: "4e21929a-36ad-11ec-8d3d-0242ad130003",
+  },
+];
+
+const idiomas = [
+  {
+    title: "Clases de inglés americano y británico",
+    description: `Clases de inglés americano y británico. Cursos, exámenes nacionales e internacionales, apoyo escolar, conversación. Solamente clases individuales, a tu ritmo y necesidad. Amplia experiencia.
+
+      Estoy en Villa Martelli, pero doy clases por videollamada de WhatsApp también.`,
+    img: "https://apollo-virginia.akamaized.net/v1/files/2j8uu9cp16q81-AR/image;s=780x0;q=60",
+    price: 500,
+    categoryId: 2,
+    userId: "4e21929a-36ad-11ec-8d3d-0242ad130003",
+  },
+  {
+    title: "Clases de Francés particulares y Traducciones",
+    description: `Clases particulares de Francés.
+
+    Soy profesora de francés recibida en la Alianza Francesa.
+    
+    Práctica de conversación para principiantes, para viajes, para diferentes empleos o trabajos, y para prepararse a exámenes de lengua francesa.
+    
+    También se realizan traducciones del idioma francés al español y del español al francés.
+    
+    Villa Urquiza, Capital Federal, Caba`,
+    img: "https://apollo-virginia.akamaized.net/v1/files/485ui3q60rnz2-AR/image;s=780x0;q=60",
+    price: 500,
+    categoryId: 2,
+    userId: "4e21929a-36ad-11ec-8d3d-0242ad130003",
+  },
+  {
+    title: "Clases de Portugues",
+    description: `Clases de portugués on-line personalizadas o en grupo.
+
+    Clases para adultos y niños.`,
+    img: "https://apollo-virginia.akamaized.net/v1/files/zjr3r4md8o6r2-AR/image;s=780x0;q=60",
+    price: 600,
+    categoryId: 2,
+    userId: "4e21929a-36ad-11ec-8d3d-0242ad130003",
+  },
+];
+
+const informatica = [
+  {
+    title: "CLASES DE EXCEL ADMINISTRATIVO CONTABLES EN MICROCENTRO",
+    description: `CLASES ONLINE O PRESENCIALES
+
+    CECAH CAPACITACIÓN
+    
+    CONSULTORA EDUCATIVA Y LABORAL OFRECE CURSOS DE INFORMÁTICA CON ORIENTACIÓN LABORAL
+    
+    CURSO QUE DICTAMOS
+    
+    AUXILIAR CONTABLE CON EXCEL ADMINISTRATIVO CONTABLE - WORD Y ACCESS
+    
+    ASISTENTE PYMES CON EXCEL ADMINISTRATIVO - WORD ADMINISTRATIVO - TANGO GESTION
+    
+    CONTAMOS CON BOLSA DE TRABAJO`,
+    img: "https://apollo-virginia.akamaized.net/v1/files/8z8dw9y32w6u3-AR/image;s=780x0;q=60",
+    price: 400,
+    categoryId: 3,
+    userId: "4e21929a-36ad-11ec-8d3d-0242ad130003",
+  },
+  {
+    title: "Clases de computación",
+    description: `Clases de computación a domicilio o a distancia.
+
+    Todas las edades.
+    
+    10 años de experiencia en el rubro`,
+    img: "https://apollo-virginia.akamaized.net/v1/files/6f61c3t98oto2-AR/image;s=780x0;q=60",
+    price: 400,
+    categoryId: 3,
+    userId: "4e21929a-36ad-11ec-8d3d-0242ad130003",
+  },
+  {
+    title: "Cursos de Tango Gestion",
+    description: `Cursos de Tango gestión - Excel - Word - Access - Asistente pymes - auxiliar contable`,
+    img: "https://apollo-virginia.akamaized.net/v1/files/wuhdowebkh5l-AR/image;s=780x0;q=60",
+    price: 300,
+    categoryId: 3,
+    userId: "4e21929a-36ad-11ec-8d3d-0242ad130003",
+  },
+];
+
+const musica = [
+  {
+    title: "Clases de Guitarra en Ramos Mejia",
+    description: `Clases de guitarra
+
+    (a partir de los 8 años)
+    
+    En las clases se trabaja una técnica, y teoría musical, en función al gusto musical que le interesa a cada alumno: ya sea con púa o dedos,guitarra clásica o eléctrica. En diversos géneros, se desarrollará una técnica funcional (y personal) para cada uno.
+    
+    También se preparan ingresos a conservatorios,clases de apoyo en armonía,teoría y solfeo,audio perceptiva: dictados melódicos-armónicos y rítmicos,composición.
+    
+    Las clases son en Zona Oeste,barrio de Ramos Mejía.`,
+    img: "https://apollo-virginia.akamaized.net/v1/files/x0nd9f0u9mtz2-AR/image;s=780x0;q=60",
+    price: 400,
+    categoryId: 4,
+    userId: "4e21929a-36ad-11ec-8d3d-0242ad130003",
+  },
+  {
+    title: "Curso de bongó nivel inicial, intermedio y avanzado",
+    description: `METODOLOGIA
+
+    Forma de afinación.
+    
+    Posición para tocarlos.
+    
+    Nomenclatura musical.
+    
+    Diferentes tipos de golpes:
+    
+    Tono abierto, tono apagado, tono bajo, galleta, golpe de aro apagado (martillo),
+    
+    rulo con ambas manos, apoyatura.
+    
+    Lectura por música.
+    
+    Diferentes figuraciones:
+    
+    Quintillo, doble tiempo, tresillo de semicorcheas, tresillo de negras.
+    
+    Métricas:
+    
+    Métricas impares: 5/8, 7/8, 11/8, 13/8 y zórcico 10/8.
+    
+    Métricas compuestas: 6/8, 9/8, 12/8.
+    
+    Diferentes estilos:
+    
+    Brasileños, salsa, candombe, guaguancó, bomba, cumbia, mazacote, guaracha,
+    
+    merengue, columbia, conga cubana, afrocubano, etc.
+    
+    Rellenos para tiempos lentos.
+    
+    Método de paradidles y sus técnicas de Daniel Mele.
+    
+    Método de acentuaciones de C: Wilcoxon.
+    
+    Análisis, estudio y lectura de partituras.
+    
+    Ritmos acompañando música grabada.
+    
+    Polirritmia.
+    
+    Desarrollo de la improvisación.
+    
+    (EL VALOR ES POR CLASE)`,
+    img: "https://apollo-virginia.akamaized.net/v1/files/x0nd9f0u9mtz2-AR/image;s=780x0;q=60",
+    price: 1000,
+    categoryId: 4,
+    userId: "4e21929a-36ad-11ec-8d3d-0242ad130003",
+  },
+  {
+    title: "Clases de Canto Popular Online",
+    description: `vocalización-soltura de la voz-afinación-diafragma-interpretación trabajo de la respiración-tecnica vocal-preparación para casting. trabajo escénico- audidiones- clases Individuale. zona belgrano y centro. comienzan en marzo Informate para la Inscripción.
+
+    CLASES ONLINE`,
+    img: "https://apollo-virginia.akamaized.net/v1/files/0svo3bxh8lhd3-AR/image;s=780x0;q=60",
+    price: 1500,
+    categoryId: 4,
+    userId: "4e21929a-36ad-11ec-8d3d-0242ad130003",
+  },
+];
+
+const otros = [
+  {
+    title: "clases de tenis",
+    description: `Clasese de tenis en zona de flores y Almagro, en otras zonas consultar`,
+    img: "https://apollo-virginia.akamaized.net/v1/files/zxnijuhos2ku1-AR/image;s=780x0;q=60",
+    price: 400,
+    categoryId: 5,
+    userId: "4e21929a-36ad-11ec-8d3d-0242ad130003",
+  },
+  {
+    title:
+      "TATUADOR PROFESIONAL//CENTRO EDUCATIVO ALEM( PRESENCIAL/ PROTOCOLOS SANITARIOS APROBADOS)",
+    description: `24 CLASES(DOS HORAS POR SEMANA) TEÓRICO/PRÁCTICAS
+
+    HORARIO DE CURSADA:
+    
+    MARTES DE 20 A 22 COMIENZA EL 2 DE NOVIEMBRE
+    
+    PRESENCIAL/PROTOCOLOS SANITARIOS APROBADOS/PARA INFORMES E INSCRIPCIÓN LLAMAR DELIA O DEJAR UN CELULAR Y ME COMUNICO
+    
+    CLASE 1;SEGURIDAD, HIGIENE Y CUIDADOS: Presentación del curso y alumno/Lugar de trabajo/Seguridad legal/primeros auxilios/Composición y armado del área de trabajo/ Armado de máquina/ Seguridad e higiene (infecciones directas y cruzadas)/ Desarmado del área de trabajo/Cuidados del tatuaje
+    
+    CLASE 2;DISEÑO Y STENCIL
+    
+    Armado y análisis del diseño/ Realización del stencil/Práctica:esténcil sobre piel sintética
+    
+    CLASE 3; MÁQUINAS
+    
+    Tipos de máquinas (rotativas y electromagnéticas)/ Práctica,
+    
+    Partes que componen una máquina
+    
+    Comparativa de máquinas rotativas y electromagnéticas, ventajas y desventajas/Calibrado de máquinas electromagnéticas/ Fuente de alimentación/ Agujas y punteras/Eterización
+    
+    CLASE 4;TINTAS (TIPOS, USOS, CUIDADOS, VENTAJAS Y DESVENTAJAS)
+    
+    Conocimiento de los distintos tipos de tintas/ Diluir pigmentos/ Crear nuevos tonos de pigmentos/ Práctico; Conocimiento de pigmentos negro y color, diluir los mismos, componer nuevos colores
+    
+    CLASE 5; DELINEADO
+    
+    Teoría del delineado
+    
+    Teoría de lubricación, estiramiento y limpieza de la zona que se está tatuando/Práctico aplicación de lo teórico sobre piel sintética
+    
+    CLASE 6; RELLENO Y SOMBRA (RELLENO, NEGRO, COLOR Y SOMBRAS)
+    
+    Teoría del relleno/Teoría del sombreado/ Práctico; aplicación de lo teórico sobre piel sintética
+    
+    CLASES 7 Y 8;BLACKundefinedGREY (ESCALAS GREYEASH, USOS, VENTAJAS Y DESVENTAJAS)
+    
+    Teoría del BlackundefinedGrey
+    
+    Conocimientos de la técnica mediante imágenes de artistas reconocidos
+    
+    Práctic; Aplicación de lo teórico sobre piel sintética
+    
+    Realizar Stencil de un diseño de BlackundefinedGrey simple, transferido a piel sintética y aplicar la técnica aprendida
+    
+    CLASES 9 Y 10; BLACKWORK, PUNTILLISMO, Y WATERCOLOR (ACUARELA)
+    
+    Teoría del Blackwork y de puntillismo/Conocimiento de la técnica/mediante imágenes de artistas reconocidos/ Práctico; Aplicación de lo teórico sobre piel sintética/ Realizar Stencil de un diseño tradicional simple, transferido a piel sintética y aplicar la técnica aprendida
+    
+    CLASES 11 A 12; GEOMETRÍA, ORNAMENTOS Y LETTERING
+    
+    Teoría del tatuaje geometría, ornamentos y lettering
+    
+    Conocimiento de la técnica mediante imágenes de artistas reconocidos/ Práctico; ejercicios de aplicación de lo teórico sobre piel sintética/Realizar Stencil de un diseño simple sobre piel sintética con la técnica aprendida
+    
+    CLASES 13 Y 14;TATUAJE TRADICIONAL (OLD SCHOOL)
+    
+    Teoría del tatuaje tradicional/Conocimiento de la técnica mediante imágenes de artistas reconocidos/ Práctico; Aplicación de lo teórico sobre piel sintética/ Realizar Stencil de un diseño tradicional simple, transferido a piel sintética con la técnica aprendida
+    
+    CLASES 15 A 16; TATUAJE NEOTRADICIONAL (NEW SCHOOL)Teoría del tatuaje neotradicional/Conocimiento de la técnica mediante imágenes de artistas reconocidos/ Práctico; Aplicación de lo teórico sobre piel sintética/ Realizar Stencil de un diseño neotradicional simple
+    
+    CLASES 17 Y 18TATUAJE ORIENTAL (IREZUMI)
+    
+    Teoría del tatuaje oriental/Conocimiento de la técnica mediante imágenes de artistas reconocidos/ Práctico; Aplicación de lo teórico sobre piel sintética/ Realizar Stencil de un diseño tradicional simple, transferido a piel sintética con la técnica aprendida
+    
+    CLASES 19 Y 20;TATUAJE TRIBAL (MAORI, CELTA, SAMOANOS Y MODERNOS)
+    
+    Teoría del tatuaje tribal/Conocimiento de la técnica mediante imágenes de artistas reconocidos/ Práctico; Aplicación de lo teórico sobre piel sintética
+    
+    CLASES 21 Y 22;TRASHPOLKA, SURREALISMO, REALISMO Y BOCETO
+    
+    Teoría del tatuaje trashpolka, surrealismo y boceto/Conocimiento de la técnica mediante imágenes de artistas reconocidos/Práctico/Realizar Stencil de un diseño tradicional simple, transferido a piel sintética con la técnica aprendida
+    
+    CLASE 23 MODELO VIVO
+    
+    CLASE 24 EVALUACIÓN`,
+    img: "https://apollo-virginia.akamaized.net/v1/files/s88lz22xwo4-AR/image;s=780x0;q=60",
+    price: 2000,
+    categoryId: 5,
+    userId: "4e21929a-36ad-11ec-8d3d-0242ad130003",
+  },
+  {
+    title: "CURSO ONLINE DE UÑAS ESCULPIDAS EN GEL",
+    description: `Clasese de tenis en zona de flores y Almagro, en otras zonas consultar`,
+    img: "https://apollo-virginia.akamaized.net/v1/files/v6cdpvy34sh61-AR/image;s=780x0;q=60",
+    price: 499,
+    categoryId: 5,
+    userId: "4e21929a-36ad-11ec-8d3d-0242ad130003",
+  },
+];
+
+const teatro = [
+  {
+    title: "CLASES DE TEATRO MUSICAL INFANTIL Y TEATRO PARA ADULTOS",
+    description: `CLASES DE TEATRO MUSICAL INFANTIL PRESENCIAL Y CON PROTOCOLOS.
+    TEATRO ADULTOS POR ZOOM. INSCRIPCIONES ABIERTAS!!`,
+    img: "https://scontent.fros10-1.fna.fbcdn.net/v/t1.6435-9/210760894_10225816617112601_6060376508752211218_n.jpg?_nc_cat=101&ccb=1-5&_nc_sid=843cd7&_nc_ohc=0ByYtO_Ag3AAX-hIG8_&_nc_ht=scontent.fros10-1.fna&oh=5811fe48117cedc6a4dbd89ae16a48fc&oe=61A127F0",
+    price: 1500,
+    categoryId: 6,
+    userId: "4e21929a-36ad-11ec-8d3d-0242ad130003",
+  },
+  {
+    title: "Clases de teatro",
+    description: `MAÑANA MAÑANA MAÑANAAAA
+    ¡NUEVO TALLER DE ACTUACIÓN EN VILLA MAIPÚ!
+    Desde el 13 de octubre, todos los miercoles a las 19:30hs, hasta las 20:30, en nuestro querido Club Atlético Central Argentino! 
+     Son 8 clases en las que, a traves del juego, empezamos a trabajar la confianza (en uno y en los compañeros), el soltarse, reírse de uno mismo y mientras tanto incorporamos nociones de actuación, expresión corporal y pantomima. Desde el 13 de octubre, todos los miercoles a las 19:30hs, hasta las 20:30.
+    La primera clase es gratuita y luego $200 por alumno por clase. Si te quedaron dudas consultame!
+    Y si compartís nos re ayudas. 😁❤
+    @argentino.club #teatro #actuación #clasesdeteatro #villamaipú #chicos #niños #chicas #niñas #hoy #arte #sanma #sanmartin #caseros #ballester #sanandres #vp #tbt #actividadesparachicos #actividadesinfantiles #teatroinfantil #buenprecio
+    #norte`,
+    img: "https://scontent.fros10-1.fna.fbcdn.net/v/t1.6435-9/245499215_3283265781959637_8289099926818192063_n.jpg?_nc_cat=104&ccb=1-5&_nc_sid=825194&_nc_ohc=_LiyxMIWSE0AX_8mXdb&_nc_ht=scontent.fros10-1.fna&oh=64f9ea377b103fbb54cdaf082f67ded3&oe=61A313CC",
+    price: 200,
+    categoryId: 6,
+    userId: "4e21929a-36ad-11ec-8d3d-0242ad130003",
+  },
+  {
+    title: "Teatro",
+    description: `✨🎭Clases de teatro para chicos!🎭✨
+    .
+    .
+    No dudes en consultar! 
+    .
+    .
+    
+    https://instagram.com/improvis_arte_?utm_medium=copy_link`,
+    img: "https://scontent.fros10-1.fna.fbcdn.net/v/t45.5328-4/222045037_4166717510110410_1612684538439272807_n.jpg?_nc_cat=106&ccb=1-5&_nc_sid=c48759&_nc_ohc=frPe_kGxVLcAX9rUxrL&_nc_ht=scontent.fros10-1.fna&oh=4cc85591ada701ebbdfdd90dd4f57e27&oe=61805741",
+    price: 200,
+    categoryId: 6,
+    userId: "4e21929a-36ad-11ec-8d3d-0242ad130003",
+  },
+];
+
+const academia = [
+  {
+    title:
+      "BUSCAS AYUDA PARA TU TESIS? SI SE PUEDE TE AYUDAMOS CONSULTA AHORA MONOGRAFIAS TESIS MAESTRIA",
+    description: `Necesitas finalizar tu tesis? Monografía, Manuscrito Reporte de Caso Ensayo
+
+    Practica Profesional Seminario Final de Grado Articulo
+    
+    Publicación Maestría Doctoral
+    
+    Tenemos TU SOLUCION! profesionales
+    
+    profesores, expertos te ayudamos con tu TESIS De grado , Posgrado Doctoral
+    
+    HACEMOS EXITOSA TU TESIS
+    
+    Profesionales al servicio DE LO QUE NECESITAS! TE AYUDAMOS A CONCRETARLO! ASESORIA DE TESIS REDACCION CORRECCION
+    
+    SALVA TU GRADUACION AHORA
+    
+    contáctanos via WHATSAPP HACIENDO LINK ABAJO O POR CHAT OLX.`,
+    img: "https://apollo-virginia.akamaized.net/v1/files/q46x16k7ssg92-AR/image;s=780x0;q=60",
+    price: 3500,
+    categoryId: 7,
+    userId: "4e21929a-36ad-11ec-8d3d-0242ad130003",
+  },
+  {
+    title:
+      "BUSCAS AYUDA PARA TU TESIS? SI SE PUEDE TE AYUDAMOS CONSULTA AHORA MONOGRAFIAS TESIS MAESTRIA",
+    description: `Asesoramiento Trabajos Prácticos Obligatorios y Foros
+
+    Conozco las características de funcionamiento y formato de dichos trabajos.
+    
+    Corrijo sin cargo cualquier observación.
+    
+    Trabajos hechos con calidad y responsabilidad
+    
+    No se cobran adelantos`,
+    img: "https://apollo-virginia.akamaized.net/v1/files/y1xaggqmurqp2-AR/image;s=780x0;q=60",
+    price: 3500,
+    categoryId: 7,
+    userId: "4e21929a-36ad-11ec-8d3d-0242ad130003",
+  },
+  {
+    title: "clases particulares de matemáticas",
+    description: `Dicto clases particulares de matemáticas de los niveles primario, secundario, cbc y universitario, las clases pueden ser a domicilio.`,
+    img: "https://apollo-virginia.akamaized.net/v1/files/5fc73qn6amlq1-AR/image;s=780x0;q=60",
+    price: 500,
+    categoryId: 7,
+    userId: "4e21929a-36ad-11ec-8d3d-0242ad130003",
+  },
+];
+
+const lessons = [
+  ...danzas,
+  ...idiomas,
+  ...informatica,
+  ...musica,
+  ...otros,
+  ...teatro,
+  ...academia,
+];
+
+module.exports = {
+  lessons,
+};
