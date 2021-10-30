0c19e4b98c567646e8e65d6902ff62636774d896
Facundo <86330512+Facupelli@users.noreply.github.com>
2021-10-30 17:33:46 -0300

    feat(api/mockup) added house service mockup (#130)

---
+++ api/mock/services/houseServices.js
@@ -0,0 +1,149 @@
+const gardener = [
+  {
+    title: "Jardinero Matriculado",
+    description:
+      "Proin interdum mauris non ligula pellentesque ultrices. Phasellus id sapien in sapien iaculis congue. Vivamus metus arcu, adipiscing molestie, hendrerit at, vulputate vitae, nisl.\n\nAenean lectus. Pellentesque eget nunc. Donec quis orci eget orci vehicula condimentum.\n\nCurabitur in libero ut massa volutpat convallis. Morbi odio odio, elementum eu, interdum eu, tincidunt in, leo. Maecenas pulvinar lobortis est.",
+    img: "http://dummyimage.com/149x100.png/ff4444/ffffff",
+    price: 1500,
+    categoryId: 30,
+    userId: "4e219448-36ad-11ec-8d3d-0242ac130003",
+  },
+  {
+    title: "Jardinero Profesional",
+    description:
+      "Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vivamus vestibulum sagittis sapien. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.\n\nEtiam vel augue. Vestibulum rutrum rutrum neque. Aenean auctor gravida sem.\n\nPraesent id massa id nisl venenatis lacinia. Aenean sit amet justo. Morbi ut odio.",
+    img: "http://dummyimage.com/249x100.png/dddddd/000000",
+    price: 2500,
+    categoryId: 30,
+    userId: "4e219510-36ad-11ec-8d3d-0242ac110003",
+  },
+  {
+    title: "Jardinero Junior",
+    description:
+      "In congue. Etiam justo. Etiam pretium iaculis justo.\n\nIn hac habitasse platea dictumst. Etiam faucibus cursus urna. Ut tellus.",
+    img: "http://dummyimage.com/175x100.png/dddddd/000000",
+    price: 1000,
+    categoryId: 30,
+    userId: "4e219448-36ad-11ec-8d3d-0242ac130003",
+  },
+  {
+    title: "Hago Changas en el Jardin",
+    description:
+      "Phasellus in felis. Donec semper sapien a libero. Nam dui.\n\nProin leo odio, porttitor id, consequat in, consequat ut, nulla. Sed accumsan felis. Ut at dolor quis odio consequat varius.",
+    img: "http://dummyimage.com/244x100.png/ff4444/ffffff",
+    price: 500,
+    categoryId: 30,
+    userId: "4e21929a-36ad-11ec-8d3d-0242ad130003",
+  },
+  {
+    title: "Jardinero - solo casas grandes",
+    description:
+      "Cras non velit nec nisi vulputate nonummy. Maecenas tincidunt lacus at velit. Vivamus vel nulla eget eros elementum pellentesque.\n\nQuisque porta volutpat erat. Quisque erat eros, viverra eget, congue eget, semper rutrum, nulla. Nunc purus.",
+    img: "http://dummyimage.com/178x100.png/ff4444/ffffff",
+    price: 1250,
+    categoryId: 30,
+    userId: "4e21929a-36ad-11ec-8d3d-0242ac170003",
+  },
+];
+
+const babySister = [
+  {
+    title: "Niñera cualquier horario",
+    description:
+      "Sed ante. Vivamus tortor. Duis mattis egestas metus.\n\nAenean fermentum. Donec ut mauris eget massa tempor convallis. Nulla neque libero, convallis eget, eleifend luctus, ultricies eu, nibh.",
+    img: "http://dummyimage.com/129x100.png/ff4444/ffffff",
+    price: 750,
+    categoryId: 31,
+    userId: "be19f20c-36aa-11ec-8d3d-0242ac130003",
+  },
+  {
+    title: "Niñero, solo niños buenos",
+    description:
+      "Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla. Sed vel enim sit amet nunc viverra dapibus. Nulla suscipit ligula in lacus.",
+    img: "http://dummyimage.com/211x100.png/5fa2dd/ffffff",
+    price: 1000,
+    categoryId: 31,
+    userId: "be19f554-36aa-11ec-8d3d-0242ac130003",
+  },
+  {
+    title: "Niñera toda la semana",
+    description:
+      "Proin interdum mauris non ligula pellentesque ultrices. Phasellus id sapien in sapien iaculis congue. Vivamus metus arcu, adipiscing molestie, hendrerit at, vulputate vitae, nisl.\n\nAenean lectus. Pellentesque eget nunc. Donec quis orci eget orci vehicula condimentum.",
+    img: "http://dummyimage.com/232x100.png/cc0000/ffffff",
+    price: 600,
+    categoryId: 31,
+    userId: "be19f6da-36aa-11ec-8d3d-0242ac130003",
+  },
+  {
+    title: "Niñera solo findes",
+    description:
+      "Fusce consequat. Nulla nisl. Nunc nisl.\n\nDuis bibendum, felis sed interdum venenatis, turpis enim blandit mi, in porttitor pede justo eu massa. Donec dapibus. Duis at velit eu est congue elementum.\n\nIn hac habitasse platea dictumst. Morbi vestibulum, velit id pretium iaculis, diam erat fermentum justo, nec condimentum neque sapien placerat ante. Nulla justo.",
+    img: "http://dummyimage.com/191x100.png/dddddd/000000",
+    price: 700,
+    categoryId: 31,
+    userId: "be19f8e2-36aa-11ec-8d3d-0242ac130003",
+  },
+  {
+    title: "Niñero niños de 4 a 6 años",
+    description:
+      "Mauris enim leo, rhoncus sed, vestibulum sit amet, cursus id, turpis. Integer aliquet, massa id lobortis convallis, tortor risus dapibus augue, vel accumsan tellus nisi eu orci. Mauris lacinia sapien quis libero.",
+    img: "http://dummyimage.com/194x100.png/5fa2dd/ffffff",
+    price: 500,
+    categoryId: 31,
+    userId: "4e218642-36ad-11ec-8d3d-0242ac130003",
+  },
+];
+
+const cleaningAndMaidService = [
+  {
+    title: "Limpieza de Oficinas",
+    description:
+      "Duis bibendum. Morbi non quam nec dui luctus rutrum. Nulla tellus.",
+    img: "http://dummyimage.com/227x100.png/ff4444/ffffff",
+    price: 4250,
+    categoryId: 32,
+    userId: "4e21899e-36ad-11ec-8d3d-0242ac130003",
+  },
+  {
+    title: "Limpieza de Casas Grandes",
+    description:
+      "In sagittis dui vel nisl. Duis ac nibh. Fusce lacus purus, aliquet at, feugiat non, pretium quis, lectus.",
+    img: "http://dummyimage.com/169x100.png/5fa2dd/ffffff",
+    price: 5000,
+    categoryId: 32,
+    userId: "4e218b24-36ad-11ec-8d3d-0242ac130003",
+  },
+  {
+    title: "Limpieza toda la semana",
+    description:
+      "Aenean fermentum. Donec ut mauris eget massa tempor convallis. Nulla neque libero, convallis eget, eleifend luctus, ultricies eu, nibh.\n\nQuisque id justo sit amet sapien dignissim vestibulum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nulla dapibus dolor vel est. Donec odio justo, sollicitudin ut, suscipit a, feugiat et, eros.\n\nVestibulum ac est lacinia nisi venenatis tristique. Fusce congue, diam id ornare imperdiet, sapien urna pretium nisl, ut volutpat sapien arcu sed augue. Aliquam erat volutpat.",
+    img: "http://dummyimage.com/226x100.png/cc0000/ffffff",
+    price: 2500,
+    categoryId: 32,
+    userId: "4e218c00-36ad-11ec-8d3d-0242ac130003",
+  },
+  {
+    title: "Limpieza dia por medio L,J,V",
+    description:
+      "Mauris enim leo, rhoncus sed, vestibulum sit amet, cursus id, turpis. Integer aliquet, massa id lobortis convallis, tortor risus dapibus augue, vel accumsan tellus nisi eu orci. Mauris lacinia sapien quis libero.\n\nNullam sit amet turpis elementum ligula vehicula consequat. Morbi a ipsum. Integer a nibh.",
+    img: "http://dummyimage.com/210x100.png/5fa2dd/ffffff",
+    price: 2000,
+    categoryId: 32,
+    userId: "4e218d5e-36ad-11ec-8d3d-0242ac130003",
+  },
+  {
+    title: "Limpieza de tuberias",
+    description:
+      "Proin eu mi. Nulla ac enim. In tempor, turpis nec euismod scelerisque, quam turpis adipiscing lorem, vitae mattis nibh ligula nec sem.\n\nDuis aliquam convallis nunc. Proin at turpis a pede posuere nonummy. Integer non velit.",
+    img: "http://dummyimage.com/108x100.png/cc0000/ffffff",
+    price: 5000,
+    categoryId: 32,
+    userId: "4e218e1c-36ad-11ec-8d3d-0242ac130003",
+  },
+];
+
+const houseServices = [...gardener, ...babySister, ...cleaningAndMaidService];
+
+module.exports = {
+  houseServices,
+};
