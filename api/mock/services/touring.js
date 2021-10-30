236270ef3daa0e9c74c5eb6011f2595ec4a1a55e
Facundo <86330512+Facupelli@users.noreply.github.com>
2021-10-30 18:25:22 -0300

    feat(api/mockup)added touring mockup (#132)

---
+++ api/mock/services/touring.js
@@ -0,0 +1,247 @@
+const mendoza = [
+  {
+    title: "Laguna del Diamante",
+    description:
+      "Proin leo odio, porttitor id, consequat in, consequat ut, nulla. Sed accumsan felis. Ut at dolor quis odio consequat varius.",
+    img: "http://dummyimage.com/178x100.png/dddddd/000000",
+    price: 10000,
+    categoryId: 33,
+    userId: "4e218d5e-36ad-11ec-8d3d-0242ac130003",
+  },
+  {
+    title: "Trekking Aconcagua",
+    description:
+      "Donec diam neque, vestibulum eget, vulputate ut, ultrices vel, augue. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec pharetra, magna vestibulum aliquet ultrices, erat tortor sollicitudin mi, sit amet lobortis sapien sapien non mi. Integer ac neque.\n\nDuis bibendum. Morbi non quam nec dui luctus rutrum. Nulla tellus.",
+    img: "http://dummyimage.com/187x100.png/dddddd/000000",
+    price: 20000,
+    categoryId: 33,
+    userId: "4e218d5e-36ad-11ec-8d3d-0242ac130003",
+  },
+  {
+    title: "San Rafael Cañón del Atuel",
+    description:
+      "Vestibulum quam sapien, varius ut, blandit non, interdum in, ante. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Duis faucibus accumsan odio. Curabitur convallis.",
+    img: "http://dummyimage.com/232x100.png/5fa2dd/ffffff",
+    price: 7000,
+    categoryId: 33,
+    userId: "4e218e1c-36ad-11ec-8d3d-0242ac130003",
+  },
+  {
+    title: "Las Leñas",
+    description:
+      "Pellentesque at nulla. Suspendisse potenti. Cras in purus eu magna vulputate luctus.",
+    img: "http://dummyimage.com/200x100.png/dddddd/000000",
+    price: 15000,
+    categoryId: 33,
+    userId: "4e218e1c-36ad-11ec-8d3d-0242ac130003",
+  },
+  {
+    title: "Termas de Cacheuta",
+    description:
+      "In hac habitasse platea dictumst. Etiam faucibus cursus urna. Ut tellus.\n\nNulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi.\n\nCras non velit nec nisi vulputate nonummy. Maecenas tincidunt lacus at velit. Vivamus vel nulla eget eros elementum pellentesque.",
+    img: "http://dummyimage.com/169x100.png/ff4444/ffffff",
+    price: 8000,
+    categoryId: 33,
+    userId: "4e218f98-36ad-11ec-8d3d-0242ac130003",
+  },
+];
+const misiones = [
+    {
+        title: "Cataratas del Iguazu",
+        description:
+          "Duis bibendum. Morbi non quam nec dui luctus rutrum. Nulla tellus.",
+        img: "http://dummyimage.com/227x100.png/ff4444/ffffff",
+        price: 10000,
+        categoryId: 34,
+        userId: "4e2191dc-36ad-11ec-8d3d-0242ac230003",
+      },
+      {
+        title: "Guiraoga",
+        description:
+          "In sagittis dui vel nisl. Duis ac nibh. Fusce lacus purus, aliquet at, feugiat non, pretium quis, lectus.",
+        img: "http://dummyimage.com/169x100.png/5fa2dd/ffffff",
+        price: 12500,
+        categoryId: 34,
+        userId: "4e2191dc-36ad-11ec-8d3d-0242ac230003",
+      },
+      {
+        title: "Saltos del Mocona",
+        description:
+          "Aenean fermentum. Donec ut mauris eget massa tempor convallis. Nulla neque libero, convallis eget, eleifend luctus, ultricies eu, nibh.\n\nQuisque id justo sit amet sapien dignissim vestibulum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nulla dapibus dolor vel est. Donec odio justo, sollicitudin ut, suscipit a, feugiat et, eros.\n\nVestibulum ac est lacinia nisi venenatis tristique. Fusce congue, diam id ornare imperdiet, sapien urna pretium nisl, ut volutpat sapien arcu sed augue. Aliquam erat volutpat.",
+        img: "http://dummyimage.com/226x100.png/cc0000/ffffff",
+        price: 5600,
+        categoryId: 34,
+        userId: "4e2191dc-36ad-11ec-8d3d-0242ac230003",
+      },
+      {
+        title: "Tres Fronteras",
+        description:
+          "Mauris enim leo, rhoncus sed, vestibulum sit amet, cursus id, turpis. Integer aliquet, massa id lobortis convallis, tortor risus dapibus augue, vel accumsan tellus nisi eu orci. Mauris lacinia sapien quis libero.\n\nNullam sit amet turpis elementum ligula vehicula consequat. Morbi a ipsum. Integer a nibh.",
+        img: "http://dummyimage.com/210x100.png/5fa2dd/ffffff",
+        price: 4500,
+        categoryId: 34,
+        userId: "4e219056-36ad-11ec-8d3d-0242ac130003",
+      },
+      {
+        title: "Sendero Macuco",
+        description:
+          "Proin eu mi. Nulla ac enim. In tempor, turpis nec euismod scelerisque, quam turpis adipiscing lorem, vitae mattis nibh ligula nec sem.\n\nDuis aliquam convallis nunc. Proin at turpis a pede posuere nonummy. Integer non velit.",
+        img: "http://dummyimage.com/108x100.png/cc0000/ffffff",
+        price: 11000,
+        categoryId: 34,
+        userId: "4e219056-36ad-11ec-8d3d-0242ac130003",
+      },
+];
+const cordoba = [
+    {
+        title: "Nono",
+        description:
+          "Duis bibendum. Morbi non quam nec dui luctus rutrum. Nulla tellus.",
+        img: "http://dummyimage.com/227x100.png/ff4444/ffffff",
+        price: 10000,
+        categoryId: 35,
+        userId: "4e21929a-36ad-11ec-8d3d-0242ad130003",
+      },
+      {
+        title: "Mar Chiquita",
+        description:
+          "In sagittis dui vel nisl. Duis ac nibh. Fusce lacus purus, aliquet at, feugiat non, pretium quis, lectus.",
+        img: "http://dummyimage.com/169x100.png/5fa2dd/ffffff",
+        price: 12500,
+        categoryId: 35,
+        userId: "4e21929a-36ad-11ec-8d3d-0242ad130003",
+      },
+      {
+        title: "Cerro Colorado",
+        description:
+          "Aenean fermentum. Donec ut mauris eget massa tempor convallis. Nulla neque libero, convallis eget, eleifend luctus, ultricies eu, nibh.\n\nQuisque id justo sit amet sapien dignissim vestibulum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nulla dapibus dolor vel est. Donec odio justo, sollicitudin ut, suscipit a, feugiat et, eros.\n\nVestibulum ac est lacinia nisi venenatis tristique. Fusce congue, diam id ornare imperdiet, sapien urna pretium nisl, ut volutpat sapien arcu sed augue. Aliquam erat volutpat.",
+        img: "http://dummyimage.com/226x100.png/cc0000/ffffff",
+        price: 5600,
+        categoryId: 35,
+        userId: "4e21929a-36ad-11ec-8d3d-0242ad130003",
+      },
+      {
+        title: "EL CHAMPAQUÍ",
+        description:
+          "Mauris enim leo, rhoncus sed, vestibulum sit amet, cursus id, turpis. Integer aliquet, massa id lobortis convallis, tortor risus dapibus augue, vel accumsan tellus nisi eu orci. Mauris lacinia sapien quis libero.\n\nNullam sit amet turpis elementum ligula vehicula consequat. Morbi a ipsum. Integer a nibh.",
+        img: "http://dummyimage.com/210x100.png/5fa2dd/ffffff",
+        price: 4500,
+        categoryId: 35,
+        userId: "4e21929a-36ad-11ec-8d3d-0242ac170003",
+      },
+      {
+        title: "QUEBRADA DEL CONDORITO",
+        description:
+          "Proin eu mi. Nulla ac enim. In tempor, turpis nec euismod scelerisque, quam turpis adipiscing lorem, vitae mattis nibh ligula nec sem.\n\nDuis aliquam convallis nunc. Proin at turpis a pede posuere nonummy. Integer non velit.",
+        img: "http://dummyimage.com/108x100.png/cc0000/ffffff",
+        price: 11000,
+        categoryId: 35,
+        userId: "4e21929a-36ad-11ec-8d3d-0242ac170003",
+      },
+];
+const marDelPlata = [
+    {
+        title: "Aquarium",
+        description:
+          "Duis bibendum. Morbi non quam nec dui luctus rutrum. Nulla tellus.",
+        img: "http://dummyimage.com/227x100.png/ff4444/ffffff",
+        price: 3500,
+        categoryId: 36,
+        userId: "4e219510-36ad-11ec-8d3d-0242ac110003",
+      },
+      {
+        title: "Aquasol",
+        description:
+          "In sagittis dui vel nisl. Duis ac nibh. Fusce lacus purus, aliquet at, feugiat non, pretium quis, lectus.",
+        img: "http://dummyimage.com/169x100.png/5fa2dd/ffffff",
+        price: 12500,
+        categoryId: 36,
+        userId: "4e219510-36ad-11ec-8d3d-0242ac110003",
+      },
+      {
+        title: "Puerto Mar del Plata",
+        description:
+          "Aenean fermentum. Donec ut mauris eget massa tempor convallis. Nulla neque libero, convallis eget, eleifend luctus, ultricies eu, nibh.\n\nQuisque id justo sit amet sapien dignissim vestibulum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nulla dapibus dolor vel est. Donec odio justo, sollicitudin ut, suscipit a, feugiat et, eros.\n\nVestibulum ac est lacinia nisi venenatis tristique. Fusce congue, diam id ornare imperdiet, sapien urna pretium nisl, ut volutpat sapien arcu sed augue. Aliquam erat volutpat.",
+        img: "http://dummyimage.com/226x100.png/cc0000/ffffff",
+        price: 5600,
+        categoryId: 36,
+        userId: "4e219510-36ad-11ec-8d3d-0242ac110003",
+      },
+      {
+        title: "Gruta Lourdes",
+        description:
+          "Mauris enim leo, rhoncus sed, vestibulum sit amet, cursus id, turpis. Integer aliquet, massa id lobortis convallis, tortor risus dapibus augue, vel accumsan tellus nisi eu orci. Mauris lacinia sapien quis libero.\n\nNullam sit amet turpis elementum ligula vehicula consequat. Morbi a ipsum. Integer a nibh.",
+        img: "http://dummyimage.com/210x100.png/5fa2dd/ffffff",
+        price: 4500,
+        categoryId: 36,
+        userId: "4e219448-36ad-11ec-8d3d-0242ac130003",
+      },
+      {
+        title: "Torreon del Monje",
+        description:
+          "Proin eu mi. Nulla ac enim. In tempor, turpis nec euismod scelerisque, quam turpis adipiscing lorem, vitae mattis nibh ligula nec sem.\n\nDuis aliquam convallis nunc. Proin at turpis a pede posuere nonummy. Integer non velit.",
+        img: "http://dummyimage.com/108x100.png/cc0000/ffffff",
+        price: 11000,
+        categoryId: 36,
+        userId: "4e219448-36ad-11ec-8d3d-0242ac130003",
+      },
+];
+const bariloche = [
+    {
+        title: "Parque Nacional Nahuel Huapi",
+        description:
+          "Duis bibendum. Morbi non quam nec dui luctus rutrum. Nulla tellus.",
+        img: "http://dummyimage.com/227x100.png/ff4444/ffffff",
+        price: 23450,
+        categoryId: 37,
+        userId: "4e2195c4-36ad-11ec-8d3d-0242ac120003",
+      },
+      {
+        title: "Bosque de arrayanes",
+        description:
+          "In sagittis dui vel nisl. Duis ac nibh. Fusce lacus purus, aliquet at, feugiat non, pretium quis, lectus.",
+        img: "http://dummyimage.com/169x100.png/5fa2dd/ffffff",
+        price: 12670,
+        categoryId: 37,
+        userId: "4e2195c4-36ad-11ec-8d3d-0242ac120003",
+      },
+      {
+        title: "Colonia Suiza",
+        description:
+          "Aenean fermentum. Donec ut mauris eget massa tempor convallis. Nulla neque libero, convallis eget, eleifend luctus, ultricies eu, nibh.\n\nQuisque id justo sit amet sapien dignissim vestibulum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nulla dapibus dolor vel est. Donec odio justo, sollicitudin ut, suscipit a, feugiat et, eros.\n\nVestibulum ac est lacinia nisi venenatis tristique. Fusce congue, diam id ornare imperdiet, sapien urna pretium nisl, ut volutpat sapien arcu sed augue. Aliquam erat volutpat.",
+        img: "http://dummyimage.com/226x100.png/cc0000/ffffff",
+        price: 5600,
+        categoryId: 37,
+        userId: "4e2195c4-36ad-11ec-8d3d-0242ac120003",
+      },
+      {
+        title: "Circuito Chico",
+        description:
+          "Mauris enim leo, rhoncus sed, vestibulum sit amet, cursus id, turpis. Integer aliquet, massa id lobortis convallis, tortor risus dapibus augue, vel accumsan tellus nisi eu orci. Mauris lacinia sapien quis libero.\n\nNullam sit amet turpis elementum ligula vehicula consequat. Morbi a ipsum. Integer a nibh.",
+        img: "http://dummyimage.com/210x100.png/5fa2dd/ffffff",
+        price: 6700,
+        categoryId: 37,
+        userId: "4e219678-36ad-11ec-8d3d-0242ac130003",
+      },
+      {
+        title: "Lago Moreno",
+        description:
+          "Proin eu mi. Nulla ac enim. In tempor, turpis nec euismod scelerisque, quam turpis adipiscing lorem, vitae mattis nibh ligula nec sem.\n\nDuis aliquam convallis nunc. Proin at turpis a pede posuere nonummy. Integer non velit.",
+        img: "http://dummyimage.com/108x100.png/cc0000/ffffff",
+        price: 8900,
+        categoryId: 37,
+        userId: "4e219678-36ad-11ec-8d3d-0242ac130003",
+      },
+];
+
+const touring = [
+  ...mendoza,
+  ...misiones,
+  ...cordoba,
+  ...marDelPlata,
+  ...bariloche,
+];
+
+module.exports = {
+  touring,
+};
