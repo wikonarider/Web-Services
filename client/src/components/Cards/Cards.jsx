627060d425b230ef431bcc063750931f4fc0b3ee
Jonatan Segovia <jsegovia.ush@gmail.com>
2021-11-01 10:40:09 -0300

    feat/client/fixingOrderPrice done (#148)

--- client/src/components/Cards/Cards.jsx
+++ client/src/components/Cards/Cards.jsx
@@ -2,8 +2,6 @@ import CardService from '../CardService/CardService';
 import s from './Cards.module.css';
 
 export default function Cards({ services }) {
-  console.log('SERVICES', services);
-
   return (
     <div className={s.container}>
       {services &&
