2445531995f77adf64c50a3a6e9062f8f111dbaa
Fedex159 <fede.gallina94@gmail.com>
2021-11-01 13:03:35 -0300

    api/db: add alias table

--- api/src/db.js
+++ api/src/db.js
@@ -1,9 +1,9 @@
-require('dotenv').config();
-const { Sequelize } = require('sequelize');
-const fs = require('fs');
-const path = require('path');
+require("dotenv").config();
+const { Sequelize } = require("sequelize");
+const fs = require("fs");
+const path = require("path");
 const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env;
-const bcrypt = require('bcrypt');
+const bcrypt = require("bcrypt");
 
 const sequelize = new Sequelize(
   `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`,
@@ -17,13 +17,13 @@ const basename = path.basename(__filename);
 const modelDefiners = [];
 
 // Leemos todos los archivos de la carpeta Models, los requerimos y agregamos al arreglo modelDefiners
-fs.readdirSync(path.join(__dirname, '/models'))
+fs.readdirSync(path.join(__dirname, "/models"))
   .filter(
     (file) =>
-      file.indexOf('.') !== 0 && file !== basename && file.slice(-3) === '.js'
+      file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
   )
   .forEach((file) => {
-    modelDefiners.push(require(path.join(__dirname, '/models', file)));
+    modelDefiners.push(require(path.join(__dirname, "/models", file)));
   });
 
 // Injectamos la conexion (sequelize) a todos los modelos
@@ -42,13 +42,25 @@ const { Service, Users, Qualification, Category, Group, Province, City } =
   sequelize.models;
 // console.log("SEQUELIZE MODEL", sequelize.models);
 // Aca vendrian las relaciones
-Service.belongsToMany(Users, { through: 'services_users_bought' });
-Users.belongsToMany(Service, { through: 'services_users_bought' });
+Service.belongsToMany(Users, {
+  as: "servicesBought",
+  through: "services_users_bought",
+});
+Users.belongsToMany(Service, {
+  as: "servicesBought",
+  through: "services_users_bought",
+});
 
-Service.belongsToMany(Users, { through: 'services_users_favourites' });
-Users.belongsToMany(Service, { through: 'services_users_favourites' });
+Service.belongsToMany(Users, {
+  as: "servicesFavs",
+  through: "services_users_favourites",
+});
+Users.belongsToMany(Service, {
+  as: "servicesFavs",
+  through: "services_users_favourites",
+});
 
-Users.hasMany(Service);
+Users.hasMany(Service, { as: "servicesOwn" });
 Service.belongsTo(Users);
 
 Category.hasMany(Service);
@@ -73,13 +85,13 @@ City.belongsTo(Province);
 
 // Service -> n provinces
 // Provinces -> m services
-Service.belongsToMany(Province, { through: 'services_provinces' });
-Province.belongsToMany(Service, { through: 'services_provinces' });
+Service.belongsToMany(Province, { through: "services_provinces" });
+Province.belongsToMany(Service, { through: "services_provinces" });
 
 // Service -> n cities
 // City -> m services
-Service.belongsToMany(City, { through: 'services_cities' });
-City.belongsToMany(Service, { through: 'services_cities' });
+Service.belongsToMany(City, { through: "services_cities" });
+City.belongsToMany(Service, { through: "services_cities" });
 // Product.hasMany(Reviews);
 
 // hooks users
