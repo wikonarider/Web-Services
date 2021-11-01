99c5c84593803800223073b4b6c75972f2d497e2
claudioCMW <cmw_17_1662013@hotmail.com>
2021-11-01 17:28:03 -0300

    1-11 17:27

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,12 @@
 import "./App.css";
 import { Route } from "react-router-dom";
 import Home from "./components/Home/Home";
-import Register from "./components/Register/Register";
-import Login from "./components/Login/Login";
 import DetailService from "./components/DetailService/DetailService";
 import YourAccount from "./components/YourAccount/YourAccount";
 import Chat from "./components/chat/chat";
 import React from "react";
 import Landing from "./components/Landing/Landing";
+import CreateService from "./components/CreateService/CreateService";
 import { useDispatch } from "react-redux";
 import { useEffect } from "react";
 import { setCookie } from "./redux/actions";
@@ -26,7 +25,6 @@ function App() {
       <Route exact path="/" component={Landing} />
       <Route exact path="/home" component={Home} />
       <Route exact path="/chat" component={Chat} />
-      <Route exact path="/register" component={Register} />
       <Route exact path="/service" component={CreateService} />
       <Route
         exact
@@ -36,7 +34,6 @@ function App() {
         }}
       />
       <Route exact path="/account" component={YourAccount} />
-      <Route exact path="/login" component={Login} />
     </div>
   );
 }
