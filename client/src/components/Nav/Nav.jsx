f1216a0ae91e0c6cc6fa5acaa8c1116f90471ece
Micaela Montero <wikonarider@gmail.com>
2021-11-01 18:19:53 -0300

    client/Home changes in views of carrousel, home, nav, searchbar, sidebar (#157)
    * client/Home changes in views of carrousel, home, nav, searchbar, sidebar
    
    * Update Home.jsx
    
    Co-authored-by: Federico Avelin <fede.gallina94@gmail.com>

--- client/src/components/Nav/Nav.jsx
+++ client/src/components/Nav/Nav.jsx
@@ -1,13 +1,31 @@
-import React, { useState, useEffect } from 'react';
-import { Box, Toolbar, Button, AppBar, Modal } from '@mui/material';
-import SearchBar from '../SearchBar/SearchBar';
-import Cart from '../Cart/Cart';
-import SideBar from '../SideBar/SideBar';
-import UserMenu from './UserMenu';
-import Login from '../Login/Login';
-import Register from '../Register/Register';
+import React, { useState, useEffect } from "react";
+import { Box, Toolbar, Button, AppBar, Modal } from "@mui/material";
+import SearchBar from "../SearchBar/SearchBar";
+import Cart from "../Cart/Cart";
+import SideBar from "../SideBar/SideBar";
+import UserMenu from "./UserMenu";
+import Login from "../Login/Login";
+import Register from "../Register/Register";
+import { makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    height: "5vh",
+  },
+  appbar: {
+    backgroundColor: "#b0bec5",
+  },
+  button: {
+    variant: "outlined",
+    size: "medium",
+  },
+}));
 
 export default function Nav() {
+  const classes = useStyles();
   const [login, setLogin] = useState(false);
   const [registerModal, setRegisterModal] = useState(false);
   const [loginModal, setLoginModal] = useState(false);
@@ -31,89 +49,87 @@ export default function Nav() {
   };
 
   return (
-    <Box sx={{ flexGrow: 1, width: '101%' }}>
-      <AppBar position="fixed" sx={{ zIndex: '9999' }}>
-        <Toolbar
-          sx={{
-            display: 'flex',
-            justifyContent: 'center',
-            flexWrap: 'wrap',
-            gap: '5px',
-          }}
-        >
-          <Box>
-            <SideBar />
-          </Box>
-          <Box sx={{ width: '50%', ml: 'auto', mr: 'auto' }}>
-            <SearchBar />
-          </Box>
-
-          {/* Register */}
-
-          {login || cookie ? null : (
-            <Button
-              variant="contained"
-              size="medium"
-              color="secondary"
-              onClick={handleRegister}
-            >
-              Register
-            </Button>
-          )}
-          <Modal
-            open={registerModal}
-            onClose={handleRegister}
-            aria-labelledby="modal-modal-title"
-            aria-describedby="modal-modal-description"
+    <div className={classes.root}>
+      <Box
+        sx={{
+          backgroundflexGrow: 1,
+          width: "101%",
+        }}
+      >
+        <AppBar position="fixed">
+          <Toolbar
+            className={classes.appbar}
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+              flexWrap: "wrap",
+              gap: "5px",
+            }}
           >
-            <Box
-              sx={{
-                display: 'flex',
-                justifyContent: 'center',
-                height: '100%',
-                width: '100%',
-              }}
-            >
-              <Register setRegisterModal={setRegisterModal} />
+            <Box>
+              <SideBar />
             </Box>
-          </Modal>
-
-          {/* Login */}
-
-          {!login && !cookie ? (
-            <Button
-              variant="contained"
-              size="medium"
-              color="secondary"
-              onClick={handleLogin}
-            >
-              Login
-            </Button>
-          ) : null}
-          <Modal
-            open={loginModal}
-            onClose={handleLogin}
-            aria-labelledby="modal-modal-title"
-            aria-describedby="modal-modal-description"
-          >
-            <Box
-              sx={{
-                display: 'flex',
-                justifyContent: 'center',
-                height: '100%',
-                width: '100%',
-              }}
-            >
-              <Login setLogin={setLogin} setLoginModal={setLoginModal} />
+            <Box sx={{ width: "80%", ml: "auto", mr: "auto" }}>
+              <SearchBar />
             </Box>
-          </Modal>
 
-          <Cart />
-          {login || cookie ? (
-            <UserMenu setLogin={setLogin} setCookie={setCookie} />
-          ) : null}
-        </Toolbar>
-      </AppBar>
-    </Box>
+            {/* Register */}
+
+            {login || cookie ? null : (
+              <Button className={classes.button} onClick={handleRegister}>
+                Register
+              </Button>
+            )}
+            <Modal
+              open={registerModal}
+              onClose={handleRegister}
+              aria-labelledby="modal-modal-title"
+              aria-describedby="modal-modal-description"
+            >
+              <Box
+                sx={{
+                  display: "flex",
+                  justifyContent: "center",
+                  height: "100%",
+                  width: "100%",
+                }}
+              >
+                <Register setRegisterModal={setRegisterModal} />
+              </Box>
+            </Modal>
+
+            {/* Login */}
+
+            {!login && !cookie ? (
+              <Button className={classes.button} onClick={handleLogin}>
+                Login
+              </Button>
+            ) : null}
+            <Modal
+              open={loginModal}
+              onClose={handleLogin}
+              aria-labelledby="modal-modal-title"
+              aria-describedby="modal-modal-description"
+            >
+              <Box
+                sx={{
+                  display: "flex",
+                  justifyContent: "center",
+                  height: "100%",
+                  width: "100%",
+                }}
+              >
+                <Login setLogin={setLogin} setLoginModal={setLoginModal} />
+              </Box>
+            </Modal>
+
+            <Cart />
+            {login || cookie ? (
+              <UserMenu setLogin={setLogin} setCookie={setCookie} />
+            ) : null}
+          </Toolbar>
+        </AppBar>
+      </Box>
+    </div>
   );
 }
