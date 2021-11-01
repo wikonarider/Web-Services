4b6113988cb330a4bea3bcba0c4b5f08aa34aedf
Franco1312 <77838417+Franco1312@users.noreply.github.com>
2021-11-01 09:47:14 -0300

    api/favs(Fix(crashFavs) (#144)

--- api/src/controllers/fav.js
+++ api/src/controllers/fav.js
@@ -34,7 +34,7 @@ async function getFavs(req, res, next) {
   }
 }
 
-async function getFavsServicesData(req,res,next){
+async function getFavsServicesData(req, res, next) {
   try {
     const { userId } = req.params;
     const userFavs = await Services_users_favourites.findAll({
@@ -77,17 +77,32 @@ async function deleteFav(req, res, next) {
 }
 
 async function validateFav(req, res, next) {
+  const { id } = req.query;
+  const { userId } = req.cookies;
+  // console.log('cookieEnBack', userId)
+  // console.log('idEnBack', id)
   try {
-    const { id } = req.query;
-    const { userId } = req.cookies;
 
-    const userFavs = await Services_users_favourites.findOne({
-      where: {
-        userId: userId,
-        serviceId: Number(id),
-      },
-    });
-    userFavs ? res.status(200).json(true) : res.status(200).json(false);
+    if (!userId){
+      res.send('You need to be logged')
+    }{
+      const userFavs = await Services_users_favourites.findOne({
+        where: {
+          userId: userId,
+          serviceId: Number(id),
+        },
+      });
+      // if (userFavs){
+      //   console.log('exitoso')
+      //   console.log('userFavs', userFavs)
+      // }
+      // else {
+      //   console.log('no hay userFavs')
+      // }
+      userFavs ? res.status(200).json(true) : res.status(200).json('noFavs');
+
+    }
+
   } catch (e) {
     next(e);
   }
--- client/src/components/DetailService/DetailService.jsx
+++ client/src/components/DetailService/DetailService.jsx
@@ -21,6 +21,7 @@ export default function DetailService({ id }) {
   //componentDidMount para traer la información del servicio por id
   useEffect(() => {
     axios(`http://localhost:3001/services/${id}`).then((response) => {
+      console.log('respuestaEnDetail', response)
       setService({ ...service, ...response.data });
     });
 
