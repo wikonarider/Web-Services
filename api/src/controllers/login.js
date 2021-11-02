3b9f9b32508f654a43cda7905c9866e72d1f2b50
Fedex159 <fede.gallina94@gmail.com>
2021-11-02 20:49:51 -0300

    api/login: delete console.log

--- api/src/controllers/login.js
+++ api/src/controllers/login.js
@@ -16,8 +16,6 @@ async function postLogin(req, res, next) {
       const checkPass = await userFinded.validPassword(password);
       if ((userFinded.username = username && checkPass === true)) {
         res.cookie("userId", userFinded.id, { expire: new Date() + 9999 }); // HttpOnly el acceso al script del cliente no esta permitido
-        var cookie = req.cookies;
-        console.log(cookie);
         res.send(userFinded.id);
       } else {
         res.status(404).send("password incorrect");
