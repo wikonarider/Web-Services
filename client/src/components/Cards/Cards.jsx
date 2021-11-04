6fd626d49a8eee55f49d608a49e6f61834253223
Fedex159 <fede.gallina94@gmail.com>
2021-11-04 19:52:35 -0300

    front/cards: change key

--- client/src/components/Cards/Cards.jsx
+++ client/src/components/Cards/Cards.jsx
@@ -1,8 +1,8 @@
-import CardService from '../CardService/CardService';
-import s from './Cards.module.css';
-import { useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { getUserInfo } from '../../redux/actions';
+import CardService from "../CardService/CardService";
+import s from "./Cards.module.css";
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { getUserInfo } from "../../redux/actions";
 
 export default function Cards({ services }) {
   const dispatch = useDispatch();
@@ -19,8 +19,11 @@ export default function Cards({ services }) {
   return (
     <div className={s.container}>
       {services &&
-        services.map((service) => (
-          <CardService key={service.id} service={service} />
+        services.map((service, i) => (
+          <CardService
+            key={`${service.id}_${service.title}_${i}`}
+            service={service}
+          />
         ))}
     </div>
   );
