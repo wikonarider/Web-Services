5ee1e12f894521293eabc37c6154c148a18669d4
Fedex159 <fede.gallina94@gmail.com>
2021-11-05 18:07:46 -0300

    front/sidebar: prettier

--- client/src/components/SideBar/SideBar.jsx
+++ client/src/components/SideBar/SideBar.jsx
@@ -1,26 +1,26 @@
-import React from 'react';
-import { useState } from 'react';
+import React from "react";
+import { useState } from "react";
 
-import SideBarNested from './SideBarNested/SideBarNested';
-import SideBarOrderPrice from './SideBarOrderPrice/SideBarOrderPrice';
-import SideBarRangePrice from './SideBarRangePrice/SideBarRangePrice';
-import SideBarRangeDate from './SideBarRangeDate/SideBarRangeDate';
-import SideBarOrderRating from './SideBarOrderRating/SideBarOrderRating';
+import SideBarNested from "./SideBarNested/SideBarNested";
+import SideBarOrderPrice from "./SideBarOrderPrice/SideBarOrderPrice";
+import SideBarRangePrice from "./SideBarRangePrice/SideBarRangePrice";
+import SideBarRangeDate from "./SideBarRangeDate/SideBarRangeDate";
+import SideBarOrderRating from "./SideBarOrderRating/SideBarOrderRating";
 
-import Box from '@mui/material/Box';
-import List from '@mui/material/List';
-import Divider from '@mui/material/Divider';
-import IconButton from '@mui/material/IconButton';
-import SortIcon from '@material-ui/icons/Sort';
-import ListItem from '@mui/material/ListItem';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import MailIcon from '@mui/icons-material/Mail';
-import Drawer from '@mui/material/Drawer';
-import Button from '@mui/material/Button';
+import Box from "@mui/material/Box";
+import List from "@mui/material/List";
+import Divider from "@mui/material/Divider";
+import IconButton from "@mui/material/IconButton";
+import SortIcon from "@material-ui/icons/Sort";
+import ListItem from "@mui/material/ListItem";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import MailIcon from "@mui/icons-material/Mail";
+import Drawer from "@mui/material/Drawer";
+import Button from "@mui/material/Button";
 
-import { setObjGlobal } from '../../redux/actions/index';
-import { useDispatch } from 'react-redux';
+import { setObjGlobal } from "../../redux/actions/index";
+import { useDispatch } from "react-redux";
 //maneja el ancho de la expansión al tocar el hamburguer button
 const drawerWidth = 350;
 
@@ -34,16 +34,16 @@ export default function MiniDrawer() {
 
   const handleReset = () => {
     const obj = {
-      startRange: '',
-      endRange: '',
+      startRange: "",
+      endRange: "",
       category: [],
-      page: '0',
-      pageSize: '20',
-      order: '',
-      type: '',
-      province: '',
-      city: '',
-      title: '',
+      page: "0",
+      pageSize: "20",
+      order: "",
+      type: "",
+      province: "",
+      city: "",
+      title: "",
     };
     dispatch(setObjGlobal(obj));
   };
@@ -62,25 +62,25 @@ export default function MiniDrawer() {
       <Drawer open={open} sx={{ width: drawerWidth }}>
         <SideBarNested openFromFather={open} />
         <Divider />
-        <SideBarOrderRating text={'Rating order'} />
+        <SideBarOrderRating text={"Rating order"} />
         <Divider />
-        <SideBarOrderPrice text={'Price order'} />
+        <SideBarOrderPrice text={"Price order"} />
         <Divider />
-        <SideBarRangeDate text={'Date order'} />
+        <SideBarRangeDate text={"Date order"} />
         <Divider />
         <SideBarRangePrice />
         <Divider />
         <Button
           variant="outlined"
           onClick={handleReset}
-          sx={{ width: '20%', margin: '10px auto 10px auto' }}
+          sx={{ width: "20%", margin: "10px auto 10px auto" }}
           color="secondary"
         >
           Reset
         </Button>
         <Divider />
         <List>
-          {['Info', 'About', 'Something'].map((text, index) => (
+          {["Info", "About", "Something"].map((text, index) => (
             <ListItem button key={index}>
               <ListItemIcon>
                 <MailIcon />
