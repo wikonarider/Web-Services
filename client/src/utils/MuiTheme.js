130dc70bdfb764591a87dbbfab363e5bfa44f2b7
Federico Avelin <fede.gallina94@gmail.com>
2021-11-05 14:59:20 -0300

    Update MuiTheme.js

--- client/src/utils/MuiTheme.js
+++ client/src/utils/MuiTheme.js
@@ -8,7 +8,7 @@
 //   green,
 // } from '@mui/material/colors';
 
-<<<<<<< HEAD
+
 // const theme = createTheme({
 //   palette: {
 //     primary: {
@@ -24,22 +24,6 @@
 //     succcess: green,
 //   },
 // });
-=======
-const theme = createTheme({
-  palette: {
-    primary: {
-      main: blueGrey[100],
-      contrastText: brown[500],
-    },
-    secondary: {
-      main: lime[600],
-      contrastText: brown[500],
-    },
-    error: deepOrange,
-    warning: amber,
-    succcess: green,
-  },
-});
->>>>>>> 615b56c91e8b9459cccd9b8b77a6e50740e7d7d6
+
 
 // export default theme;
