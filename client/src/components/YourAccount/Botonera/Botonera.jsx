f9045d7c57e59818dd801c42a269360be78a90b6
Facundo <86330512+Facupelli@users.noreply.github.com>
2021-11-08 12:05:47 -0300

    fix account styles, divider and flex start (#269)

--- client/src/components/YourAccount/Botonera/Botonera.jsx
+++ client/src/components/YourAccount/Botonera/Botonera.jsx
@@ -4,10 +4,11 @@ import PostAddIcon from "@mui/icons-material/PostAdd";
 import ShoppingBagIcon from "@mui/icons-material/ShoppingBag";
 import DataSaverOffIcon from "@mui/icons-material/DataSaverOff";
 import VisibilityIcon from "@mui/icons-material/Visibility";
-import AdminPanelSettingsIcon from '@mui/icons-material/AdminPanelSettings';
+import AdminPanelSettingsIcon from "@mui/icons-material/AdminPanelSettings";
 
 import s from "./Botonera.module.css";
 import { useHistory } from "react-router";
+import { Divider } from "@mui/material";
 
 export default function YourAccount({
   viewServices,
@@ -31,88 +32,97 @@ export default function YourAccount({
     marginRight: 1,
     marginLeft: 1,
     marginBottom: 1,
-  }
+  };
 
   return (
-    <div className={s.botonera}>
-      {user.admin && (
+    <>
+      <div className={s.botonera}>
+        {user.admin && (
+          <Button
+            variant={viewAdmin ? "contained" : "outlined"}
+            color={viewAdmin ? "secondary" : "primary"}
+            startIcon={<AdminPanelSettingsIcon />}
+            onClick={() => {
+              setViewFavs(false);
+              setViewOrders(false);
+              setViewAdmin(!viewAdmin);
+              setViewservices(false);
+            }}
+            sx={buttonStyle}
+          >
+            Admin
+          </Button>
+        )}
         <Button
-          variant={viewAdmin ? "contained" : "outlined"}
-          color={viewAdmin ? "secondary" : "primary"}
-          startIcon={<AdminPanelSettingsIcon />}
+          variant={viewOrders ? "contained" : "outlined"}
+          color={viewOrders ? "secondary" : "primary"}
+          startIcon={<ShoppingBagIcon />}
           onClick={() => {
             setViewFavs(false);
-            setViewOrders(false);
-            setViewAdmin(!viewAdmin);
+            setViewOrders(!viewOrders);
             setViewservices(false);
+            setViewAdmin(false);
           }}
           sx={buttonStyle}
         >
-          Admin
+          Your Orders
         </Button>
-      )}
-      <Button
-        variant={viewOrders ? "contained" : "outlined"}
-        color={viewOrders ? "secondary" : "primary"}
-        startIcon={<ShoppingBagIcon />}
-        onClick={() => {
-          setViewFavs(false);
-          setViewOrders(!viewOrders);
-          setViewservices(false);
-          setViewAdmin(false);
-        }}
-        sx={buttonStyle}
-      >
-        Your Orders
-      </Button>
-      <Button
-        variant={viewFavs ? "contained" : "outlined"}
-        color={viewFavs ? "secondary" : "primary"}
-        startIcon={<FavoriteIcon />}
-        onClick={() => {
-          setViewFavs(!viewFavs);
-          setViewOrders(false);
-          setViewservices(false);
-          setViewAdmin(false);
-        }}
-        sx={buttonStyle}
-      >
-        Your Favs
-      </Button>
-      <Button
-        variant={viewServices ? "contained" : "outlined"}
-        color={viewServices ? "secondary" : "primary"}
-        startIcon={<VisibilityIcon />}
-        onClick={() => {
-          setViewFavs(false);
-          setViewOrders(false);
-          setViewservices(!viewServices);
-          setViewAdmin(false);
-        }}
-        sx={buttonStyle}
-      >
-        Your Services
-      </Button>
+        <Button
+          variant={viewFavs ? "contained" : "outlined"}
+          color={viewFavs ? "secondary" : "primary"}
+          startIcon={<FavoriteIcon />}
+          onClick={() => {
+            setViewFavs(!viewFavs);
+            setViewOrders(false);
+            setViewservices(false);
+            setViewAdmin(false);
+          }}
+          sx={buttonStyle}
+        >
+          Your Favs
+        </Button>
+        <Button
+          variant={viewServices ? "contained" : "outlined"}
+          color={viewServices ? "secondary" : "primary"}
+          startIcon={<VisibilityIcon />}
+          onClick={() => {
+            setViewFavs(false);
+            setViewOrders(false);
+            setViewservices(!viewServices);
+            setViewAdmin(false);
+          }}
+          sx={buttonStyle}
+        >
+          Your Services
+        </Button>
 
-      <Button
-        variant="outlined"
-        startIcon={<PostAddIcon />}
-        sx={buttonStyle}
-        onClick={handleClickPostService}
-      >
-        Post Service
-      </Button>
+        <Button
+          variant="outlined"
+          startIcon={<PostAddIcon />}
+          sx={buttonStyle}
+          onClick={handleClickPostService}
+        >
+          Post Service
+        </Button>
 
-      <Button
-        variant="outlined"
-        startIcon={<DataSaverOffIcon />}
-        onClick={() => {
-          setOpenForm(true);
+        <Button
+          variant="outlined"
+          startIcon={<DataSaverOffIcon />}
+          onClick={() => {
+            setOpenForm(true);
+          }}
+          sx={buttonStyle}
+        >
+          Change Your Data
+        </Button>
+      </div>
+      <Divider
+        sx={{
+          border: "1px solid #cfd8dc",
+          borderRadius: 12,
+          marginBottom: "3%",
         }}
-        sx={buttonStyle}
-      >
-        Change Your Data
-      </Button>
-    </div>
+      />
+    </>
   );
 }
