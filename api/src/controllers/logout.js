789ffed0778622bb280f2fff50aa674d05247afd
Fedex159 <fede.gallina94@gmail.com>
2021-11-08 18:58:18 -0300

    api/logout: fix heroku logout

--- api/src/controllers/logout.js
+++ api/src/controllers/logout.js
@@ -1,14 +1,16 @@
-const { Users } = require('../db.js');
-
-
+const { Users } = require("../db.js");
 
 async function postLogout(req, res, next) {
-    
-    res.clearCookie('userId');
-    res.clearCookie('G_AUTHUSER_H');
-   res.send("user logout")
+  res.clearCookie("userId");
+  res.clearCookie("G_AUTHUSER_H");
+  res.cookie("userId", "", {
+    domain: "https://pf-web-services.herokuapp.com",
+    maxAge: 0,
+    overwrite: true,
+  });
+  res.send("user logout");
 }
 
 module.exports = {
-    postLogout,
-  };
+  postLogout,
+};
