2b17b3ab6ec0fd4d699637946dbd4cb7cdbbecd1
Fedex159 <fede.gallina94@gmail.com>
2021-11-08 19:44:29 -0300

    api/logout: test heroku config

--- api/src/controllers/logout.js
+++ api/src/controllers/logout.js
@@ -3,11 +3,7 @@ const { Users } = require("../db.js");
 async function postLogout(req, res, next) {
   res.clearCookie("userId");
   res.clearCookie("G_AUTHUSER_H");
-  res.cookie("userId", "", {
-    domain: "https://pf-web-services.herokuapp.com",
-    maxAge: 0,
-    overwrite: true,
-  });
+  res.setHeader("set-cookie", "userId=; max-age=0");
   res.send("user logout");
 }
 
