dbb7b9ed75acf920e4854b075a99660262119f21
Sebastian Tobar <86356868+stobar93@users.noreply.github.com>
2021-11-08 11:28:55 -0500

    Feat/users admin (#270)
    * Mock servicios comprados
    
    * Busqueda de usuario por id, name, lastname, email
    
    * Panel usuarios admin, muestra informacion usuarios
    
    * Users admin panel
    
    * Boton guardar desactiva modo edicion

--- client/src/components/Admin/Controllers/TextInput.jsx
+++ client/src/components/Admin/Controllers/TextInput.jsx
@@ -1,13 +1,30 @@
 import * as React from "react";
 
 import TextField from "@mui/material/TextField";
+import Input from "@mui/material/Input";
+export default function TextInput({
+  defaultValue,
+  setEdit,
+  edit,
+  optionId,
+  optionProp,
+}) {
+  const handleChange = (event, id) => {
+    let value = event.target.value;
+    setEdit((edit) => {
+      return { ...edit, [id]: { ...edit[id], [optionProp]: value } };
+    });
+  };
 
-export default function TextInput({ defaultValue }) {
   return (
-    <TextField
+    <Input
       id="outlined-size-small"
-      defaultValue={defaultValue}
+      placeholder={defaultValue}
+      autoComplete="off"
       size="small"
+      sx={{ width: `${(defaultValue.length + 1) * 8}px`, ml: "10px" }}
+      onChange={(e) => handleChange(e, optionId)}
+      value={edit[optionId][optionProp]}
     />
   );
 }
