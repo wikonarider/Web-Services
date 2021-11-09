676a981eaee4e534a524cd26e9c9d9ab03c08b0b
Fedex159 <fede.gallina94@gmail.com>
2021-11-09 11:53:07 -0300

    api/auth: google endpoint added

--- api/src/routes/auth.js
+++ api/src/routes/auth.js
@@ -1,7 +1,11 @@
 var express = require("express");
 var router = express.Router();
-const { generateToken } = require("../controllers/authentication");
+const {
+  generateToken,
+  googleAuthentication,
+} = require("../controllers/authentication");
 
 router.post("/login", generateToken);
+router.post("/login", googleAuthentication);
 
 module.exports = router;
