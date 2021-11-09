970b1684347647ade63f8756e12272fc608dd19c
Fedex159 <fede.gallina94@gmail.com>
2021-11-09 11:27:40 -0300

    front/utils: login change

--- client/src/utils/login.js
+++ client/src/utils/login.js
@@ -1,11 +1,6 @@
 import axios from "axios";
 
 export async function postLogin(body) {
-  var json = await axios.post(`/login`, body);
-  return json;
-}
-
-export async function postLogout() {
-  var json = await axios.post(`/logout`);
-  return json;
+  const res = await axios.post(`/auth/login`, body);
+  return res.data;
 }
