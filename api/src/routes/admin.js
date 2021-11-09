a42982a2dbc4bac4247478d29feb23c50328563c
Fedex159 <fede.gallina94@gmail.com>
2021-11-09 10:41:22 -0300

    api/admin: delete old code

--- api/src/routes/admin.js
+++ api/src/routes/admin.js
@@ -1,12 +1,7 @@
 var express = require("express");
 var router = express.Router();
 var { admin } = require("../controllers/admin.js");
-const {
-  isAuthenticated,
-  isNotAuthenticated,
-} = require("../controllers/authentication");
 
-// isAuthenticated, isAdmin,
 router.get("/", admin);
 
 module.exports = router;
