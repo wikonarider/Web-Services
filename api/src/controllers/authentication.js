5e854cc852a567418e9090abb94e2a0c18aca042
Fedex159 <fede.gallina94@gmail.com>
2021-11-09 12:18:41 -0300

    api/authentication: add try and catch

--- api/src/controllers/authentication.js
+++ api/src/controllers/authentication.js
@@ -6,63 +6,71 @@ const { OAuth2Client } = require("google-auth-library");
 const client = new OAuth2Client(process.env.CLIENT_ID);
 
 async function generateToken(req, res, next) {
-  const { username, password } = req.body;
-  const { token } = req.query;
-  if (token) {
-    next();
-    return;
-  }
-  if (username && password) {
-    // Busco el usuario
-    const user = await Users.findOne({
-      where: {
-        username: username,
-      },
-    });
-    if (user) {
-      const check = await user.validPassword(password);
-      if (check) {
-        const accessToken = jwt.sign({ id: user.id }, SECRET_KEY);
-        res.json({
-          id: user.id,
-          token: accessToken,
-        });
+  try {
+    const { username, password } = req.body;
+    const { token } = req.query;
+    if (token) {
+      next();
+      return;
+    }
+    if (username && password) {
+      // Busco el usuario
+      const user = await Users.findOne({
+        where: {
+          username: username,
+        },
+      });
+      if (user) {
+        const check = await user.validPassword(password);
+        if (check) {
+          const accessToken = jwt.sign({ id: user.id }, SECRET_KEY);
+          res.json({
+            id: user.id,
+            token: accessToken,
+          });
+        } else {
+          res.status(400).json({ message: "Invalid password" });
+        }
       } else {
-        res.status(400).json({ message: "Invalid password" });
+        res.status(400).json({ message: "User does not exist" });
       }
     } else {
-      res.status(400).json({ message: "User does not exist" });
+      res.status(400).json({ message: "Username and password is required" });
     }
-  } else {
-    res.status(400).json({ message: "Username and password is required" });
+  } catch (e) {
+    next(e);
   }
 }
 
 async function googleAuthentication(req, res, next) {
-  const { token } = req.query;
+  try {
+    const { token } = req.query;
 
-  const ticket = await client.verifyIdToken({
-    idToken: token,
-    audience:
-      "316128007785-fif02sojlsoinu9s5eugus3qaagiclid.apps.googleusercontent.com",
-  });
-
-  const { email } = ticket.getPayload();
-
-  const user = await Users.findOne({
-    where: {
-      email: email,
-    },
-  });
-
-  if (user) {
-    const accessToken = jwt.sign({ id: user.id }, SECRET_KEY);
-    res.json({
-      id: user.id,
-      token: accessToken,
+    const ticket = await client.verifyIdToken({
+      idToken: token,
+      audience:
+        "316128007785-fif02sojlsoinu9s5eugus3qaagiclid.apps.googleusercontent.com",
     });
-  } else {
-    res.status(400).json({ message: "Unregistered user" });
+
+    const { email } = ticket.getPayload();
+
+    const user = await Users.findOne({
+      where: {
+        email: email,
+      },
+    });
+
+    if (user) {
+      const accessToken = jwt.sign({ id: user.id }, SECRET_KEY);
+      res.json({
+        id: user.id,
+        token: accessToken,
+      });
+    } else {
+      res.status(400).json({ message: "Unregistered user" });
+    }
+  } catch (e) {
+    next(e);
   }
 }
 
