44e4ff2af8a3d19ec1b5d3f614815e81d275c67a
Fedex159 <fede.gallina94@gmail.com>
2021-11-09 10:39:30 -0300

    api/authentication: jwt configured

--- api/src/controllers/authentication.js
+++ api/src/controllers/authentication.js
@@ -1,33 +1,53 @@
+require("dotenv").config();
 const { Users } = require("../db");
+const jwt = require("jsonwebtoken");
+const { SECRET_KEY } = process.env;
 
-async function isAuthenticated(req, res, next) {
-  try {
-    const { userId } = req.cookies;
-    if (userId) {
-      const user = await Users.findByPk(userId);
-      if (user) {
-        next();
+async function generateToken(req, res, next) {
+  const { username, password } = req.body;
+  if (username && password) {
+    // Busco el usuario
+    const user = await Users.findOne({
+      where: {
+        username: username,
+      },
+    });
+    if (user) {
+      const check = await user.validPassword(password);
+      if (check) {
+        const accessToken = jwt.sign({ id: user.id }, SECRET_KEY);
+        res.json({
+          id: user.id,
+          token: accessToken,
+        });
       } else {
-        res.status(400).json({ data: "User not found" });
+        res.status(400).json({ message: "Invalid password" });
       }
     } else {
-      res.status(400).json({ data: "User not logged in" });
+      res.status(400).json({ message: "User does not exist" });
     }
-  } catch (e) {
-    next(e);
+  } else {
+    res.status(400).json({ message: "Username and password is required" });
   }
 }
 
-function isNotAuthenticated(req, res, next) {
-  const { userId } = req.cookies;
-  if (!userId) {
-    next();
+function verifyToken(req, res, next) {
+  const authHeader = req.headers.authorization;
+  if (authHeader) {
+    const token = authHeader.split(" ")[1];
+    jwt.verify(token, SECRET_KEY, (err, user) => {
+      if (err) {
+        return res.status(401).json({ message: "Token is not valid" });
+      }
+      req.user = user.id;
+      next();
+    });
   } else {
-    res.status(400).json({ data: "Logged in user, access not allowed" });
+    res.status(401).json({ message: "You are not authenticated" });
   }
 }
 
 module.exports = {
-  isAuthenticated,
-  isNotAuthenticated,
+  generateToken,
+  verifyToken,
 };
