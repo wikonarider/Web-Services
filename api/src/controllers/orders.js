463f7e2c3294972055251bd0c0510006c7e8c921
Fedex159 <fede.gallina94@gmail.com>
2021-11-10 18:26:30 -0300

    api/controller: add order GET and POST

--- api/src/controllers/orders.js
+++ api/src/controllers/orders.js
@@ -1,5 +1,65 @@
-const { Orders } = require('../db');
-const { validServiceId } = require('../utils/validOrders');
+const { Orders, Service } = require('../db');
+const { validServiceId, validServicesId } = require('../utils/validOrders');
+
+async function getOrder(req, res, next) {
+  try {
+    const userId = req.user;
+    const order = await Orders.findOne({
+      where: {
+        userId: userId,
+        status: 'carrito',
+      },
+    });
+    if (order) {
+      const services = await Service.findAll({
+        attributes: ['title', 'price', 'id', 'img'],
+        where: {
+          id: order.services,
+        },
+      });
+      res.json(services);
+    } else {
+      res.status(404).json({ message: 'Order not found' });
+    }
+  } catch (e) {
+    next(e);
+  }
+}
+
+async function createOrder(req, res, next) {
+  try {
+    const userId = req.user;
+    const { servicesId } = req.body;
+
+    if (servicesId) {
+      if (await validServicesId(userId, servicesId)) {
+        const [order, created] = await Orders.findOrCreate({
+          where: {
+            userId: userId,
+            status: 'carrito',
+          },
+        });
+        let message = '';
+        order.services = servicesId;
+        await order.save();
+        if (created) {
+          message = 'Successfully created order';
+        } else {
+          message = 'Successfully modified order';
+        }
+        res.json({ message });
+      } else {
+        res.status(400).json({
+          message: 'Enter a valids services or that is not the owner',
+        });
+      }
+    } else {
+      res.status(403).json({ message: 'ServicesId is required' });
+    }
+  } catch (e) {
+    next(e);
+  }
+}
 
 async function addServiceToOrder(req, res, next) {
   try {
@@ -8,29 +68,27 @@ async function addServiceToOrder(req, res, next) {
 
     if (serviceId) {
       if (await validServiceId(userId, serviceId)) {
-        const [order, created] = await Orders.findOrCreate({
+        const order = await Orders.findOne({
           where: {
             userId: userId,
-            status: 'pending',
+            status: 'carrito',
           },
         });
-        let message = '';
 
-        if (created) {
-          order.services = [serviceId];
-          message = 'Successfully added';
-        } else {
+        if (order) {
           const index = order.services.indexOf(serviceId);
           if (index === -1) {
             order.services = [...order.services, serviceId];
-            message = 'Successfully added';
+            await order.save();
+            res.json({ message: 'Successfully added', serviceId });
           } else {
-            message = 'It was not added, it already exists';
+            res
+              .status(403)
+              .json({ message: 'It was not added, it already exists' });
           }
+        } else {
+          res.status(404).json({ message: 'Order not found' });
         }
-        await order.save();
-
-        res.json({ message: message, services: order.services });
       } else {
         res
           .status(400)
@@ -52,23 +110,22 @@ async function removeServiceFromOrder(req, res, next) {
     if (serviceId) {
       const order = await Orders.findOne({
         where: {
-          status: 'pending',
+          status: 'carrito',
           userId: userId,
         },
       });
 
       if (order) {
         const arr = order.services.filter((id) => id !== serviceId);
-        let message = '';
         if (arr.length === order.services.length) {
-          message = 'Service not removed, did not exist';
+          res
+            .status(400)
+            .json({ message: 'Service not removed, did not exist' });
         } else {
-          message = 'Successfully removed';
+          order.services = arr;
+          await order.save();
+          res.json({ message: 'Successfully removed', serviceId });
         }
-        order.services = arr;
-        await order.save();
-
-        res.json({ message: message, serviceId: serviceId });
       } else {
         res.status(404).json({ message: 'Order not found' });
       }
@@ -81,6 +138,8 @@ async function removeServiceFromOrder(req, res, next) {
 }
 
 module.exports = {
+  getOrder,
+  createOrder,
   addServiceToOrder,
   removeServiceFromOrder,
 };
