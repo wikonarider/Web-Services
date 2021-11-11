60f572ee37ea0d83da0e229476e649f706476a3b
Fedex159 <fede.gallina94@gmail.com>
2021-11-11 19:05:11 -0300

    api/routes: index prettier

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,21 +1,20 @@
-const { Router } = require("express");
-const services = require("./services");
-const users = require("./users");
-const qualification = require("./qualification");
-const groups = require("./groups");
-const favs = require("./favs");
-const provinces = require("./provinces");
-const comment = require("./comment");
-const checkout = require("./checkout");
-const chat = require("./Chat");
-const admin = require("./admin");
-const forgotPassword = require("./forgotPassword")
+const { Router } = require('express');
+const services = require('./services');
+const users = require('./users');
+const qualification = require('./qualification');
+const groups = require('./groups');
+const favs = require('./favs');
+const provinces = require('./provinces');
+const comment = require('./comment');
+const checkout = require('./checkout');
+const chat = require('./Chat');
+const admin = require('./admin');
+const forgotPassword = require('./forgotPassword');
 
-const paypal = require("./paypal");
-const auth = require("./auth");
-const { verifyToken } = require("../controllers/authentication");
+const paypal = require('./paypal');
+const auth = require('./auth');
+const { verifyToken } = require('../controllers/authentication');
 const orders = require('./orders');
-
 
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
@@ -25,13 +24,7 @@ const router = Router();
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
 
-
-
-
-
-
-
-router.use("/forgotPassword", forgotPassword)
+router.use('/forgotPassword', forgotPassword);
 router.use('/services', services);
 router.use('/users', users);
 router.use('/qualification', qualification);
@@ -41,10 +34,9 @@ router.use('/auth', auth);
 router.use('/provinces', provinces);
 router.use('/comment', comment);
 router.use('/checkout', checkout);
-router.use("/chat", verifyToken, chat);
+router.use('/chat', verifyToken, chat);
 router.use('/admin', admin);
 router.use('/paypal', paypal);
 router.use('/orders', orders);
-
 
 module.exports = router;
