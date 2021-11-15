841fc99bcc245bcaa55c235a59db4b6605e98e07
Franco1312 <77838417+Franco1312@users.noreply.github.com>
2021-11-15 12:59:56 -0300

    Botones share funcionales (#365)

--- client/src/components/DetailService/RightInfoBar/RightInfoBar.jsx
+++ client/src/components/DetailService/RightInfoBar/RightInfoBar.jsx
@@ -8,6 +8,8 @@ import { AddShoppingCart, Favorite, Share } from "@mui/icons-material";
 import CardUser from "../../CardUser/CardUser";
 import useMediaQuery from "@mui/material/useMediaQuery";
 import MapDetail from "./MapDetail";
+import ShareServiceModal from "../../CardService/ShareServiceModal"
+import { useState} from 'react';
 
 export default function RightInfoBar({
   cookie,
@@ -17,9 +19,11 @@ export default function RightInfoBar({
   handleClick,
   added,
 }) {
+
   let { title, price, description, rating, qualifications, userId, city, id } =
     service.service;
   const query = useMediaQuery("(max-width: 890px)");
+  const [modal, setModal] = useState(false)
 
   return (
     <Box
@@ -55,7 +59,8 @@ export default function RightInfoBar({
               <Favorite color={favState ? "error" : ""} />
             </IconButton>
           ) : null}
-          <IconButton aria-label="share">
+          <IconButton aria-label="share"
+          onClick={() => setModal(true)}>
             <Share />
           </IconButton>
         </Box>
@@ -173,6 +178,7 @@ export default function RightInfoBar({
         />
       </Box>
       {/* -------------------------------------- */}
+      <ShareServiceModal modal={modal} setModal={setModal} serviceId={id} />
     </Box>
   );
 }
