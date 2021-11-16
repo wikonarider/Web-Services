1ffeb655678004f4f9d1dc7a1080881b35045f6e
Jonatan Segovia <jsegovia.ush@gmail.com>
2021-11-16 10:01:58 -0300

    Feat/share service modal (#380)
    * feat/shareModal centrado correctamente
    
    * feat/singleComment img reaparada para que no se achique
    
    * feat/share cambio en color see more

--- client/src/components/CardService/ShareServiceModal.jsx
+++ client/src/components/CardService/ShareServiceModal.jsx
@@ -21,6 +21,7 @@ const style = {
   boxShadow: 24,
   borderRadius: 2,
   p: 4,
+  textAlign: 'center',
 };
 
 export default function ShareServiceModal({ modal, setModal, serviceId }) {
@@ -33,28 +34,37 @@ export default function ShareServiceModal({ modal, setModal, serviceId }) {
       <Modal
         open={modal}
         onClose={handleClose}
-        aria-labelledby='modal-modal-title'
-        aria-describedby='modal-modal-description'
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
       >
         <Box sx={style}>
           <h3>Share with</h3>
-          <FacebookShareButton
-            url={`https://pf-web-service.vercel.app/services/${serviceId}`}
-         >
-            <FacebookIcon size={35} round={true} />
-          </FacebookShareButton>
+          <Box
+            sx={{
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              gap: '1.5rem',
+            }}
+          >
+            <FacebookShareButton
+              url={`https://pf-web-service.vercel.app/services/${serviceId}`}
+            >
+              <FacebookIcon size={35} round={true} />
+            </FacebookShareButton>
 
-          <WhatsappShareButton
-            url={`https://pf-web-service.vercel.app/services/${serviceId}`}
-          >
-            <WhatsappIcon size={35} round={true} />
-          </WhatsappShareButton>
-         
-          <LinkedinShareButton
-            url={`https://pf-web-service.vercel.app/services/${serviceId}`}
-          >
-            <LinkedinIcon size={35} round={true} />
-          </LinkedinShareButton>
+            <WhatsappShareButton
+              url={`https://pf-web-service.vercel.app/services/${serviceId}`}
+            >
+              <WhatsappIcon size={35} round={true} />
+            </WhatsappShareButton>
+
+            <LinkedinShareButton
+              url={`https://pf-web-service.vercel.app/services/${serviceId}`}
+            >
+              <LinkedinIcon size={35} round={true} />
+            </LinkedinShareButton>
+          </Box>
         </Box>
       </Modal>
     </div>
